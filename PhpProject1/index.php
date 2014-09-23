79ee935ba073cd5bcc2fdf95a4a42945d67a18bf
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-09-23 19:53:22 +0000

    M0

---
+++ PhpProject1/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <form action="wishlist.php" method="GET" name="wishList">
+            Show wish list of:  <input type="text" name="user" value=""/>
+                                <input type="submit" value="Go" />
+        </form>
+    </body>
+</html>
