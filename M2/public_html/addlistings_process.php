6557121c75e08cd1df611a9423af9bfbc26fee5c
rindi <rindi@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-10-13 22:28:54 +0000

    Add listings now live.

--- M2/public_html/addlistings_process.php
+++ M2/public_html/addlistings_process.php
@@ -1,3 +1,27 @@
 <?php
-echo $_POST['']
+#echo $_POST['address'].' '.$_POST['city'].' '.$_POST['zip'].' '.$_POST['price'].' '.$_POST['rooms'].' '.$_POST['bathrooms'].' '.$_POST['description'].' ';
+include("Brain/dbconfig.php");
+
+$addr  = $_POST['address'];
+$city  = $_POST['city'];
+$zip   = $_POST['zip'];
+$price = $_POST['price'];
+$rooms = $_POST['rooms'];
+$brooms = $_POST['bathrooms'];
+$desc   = $_POST['description'];
+
+#echo $addr.' '.$city.' '.$zip.' '.$price.' '.$rooms.' '.$brooms.' '.$desc.' ';
+
+$add_house =  $pdo->prepare("INSERT INTO houses (address, city, zip, price, rooms, bathrooms, description) VALUES (:addr, :city, :zip, :price, :rooms, :brooms, :description)");
+$add_house->execute(array(  ':addr' => $addr,
+                            ':city' => $city, 
+                            ':zip' => $zip,
+                            ':price' => $price,
+                            ':rooms' => $price,
+                            ':brooms' => $brooms,
+                            ':description' => $desc
+                         )
+                    );
+                    header("Location: index.php")
+
 ?>
