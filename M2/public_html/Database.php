77bd2c512ce6c5089e2f883dcb51effebdd82e5c
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-10-25 20:11:08 +0000

    working on database

--- M2/public_html/Database.php
+++ M2/public_html/Database.php
@@ -1,45 +1,49 @@
 <?php
 require ("UserData.php");
+require ("ListingData.php");
 class Database
 {
     private $con;
+    
+    //Construct a new database object
     public function __construct()
     {
         $database = 'student_f14g03';
         $username = 'f14g03';
         $password = 'fzR-5NY-5oM-W2y';
         $host = 'localhost';
-        //'mysql:host=localhost;dbname=student_f14g03', 'f14g03','fzR-5NY-5oM-W2y'
-        //$this->con = new PDO("mysql:host=".$host.";dbname=".$database,$username,$password);
         $this->con = new PDO("mysql:host=".$host.";dbname=".$database,$username,$password);
     }
+    
+    //Get Users from the database
     public function getUsers()
     {
         $sql = "SELECT * from usertable";
         foreach ($this->con->query($sql) as $row) 
         {
-            print $row['username'] . "\t";
-            print $row['password'] . "\t";
-            print $row['userid'] . "\t";
-            print $row['type'] . "\t";
-            print $row['userid'] . "\n";
+            $dataSet[] = new UserData($row);
         }
-//        $statement = $this->con->prepare($sql);
-//        $statement-> execute();
-//        while($row = $statement -> fetch())
-//        {
-//            $dataSet[] = new UserData($row);
-//        }
         if (!empty($dataSet))
             return $dataSet;
         else
             return null;
     }
+    
+    //Get housese from the database
+    public function searchListings($input)
+    {
+        $sql = "SELECT * from houses WHERE city = '$input' OR zip = '$input'";
+        foreach ($this->con->query($sql) as $row) 
+        {
+            $dataSet[] = new ListingData($row);
+        }
+        if (!empty($dataSet))
+            return $dataSet;
+        else
+            return null;
+    }
+    
 }
 ?>
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
--- M2/public_html/UserData.php
+++ M2/public_html/UserData.php
@@ -6,11 +6,11 @@ class UserData
     private $username, $password, $userid, $type, $email;
     public function __construct($dbRow)
     {
-        $this->username = $dbRow('username');
-        $this->password = $dbRow('password');
-        $this->userid = $dbRow('userid');
-        $this->email = $dbRow('type');
-        $this->type = $dbRow('email');
+        $this->username = $dbRow['username'];
+        $this->password = $dbRow['password'];
+        $this->userid = $dbRow['userid'];
+        $this->email = $dbRow['type'];
+        $this->type = $dbRow['email'];
         
     }
     public function getUserName()
@@ -34,10 +34,5 @@ class UserData
         return $this->email;
     }
 }
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 ?>
