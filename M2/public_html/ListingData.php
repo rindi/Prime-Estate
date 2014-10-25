d5a51cef943f89620a35af25c74a5b36c885879d
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-10-25 20:11:08 +0000

    working on database

---
+++ M2/public_html/ListingData.php
@@ -0,0 +1,67 @@
+<?php
+
+class ListingData
+{
+    //fields for userdata class
+    private $id, $address, $city, $zip, $price, $rooms, $bathrooms, $description,$when_added, $when_modified;
+    public function __construct($dbRow)
+    {
+        $this->id = $dbRow['id'];
+        $this->address = $dbRow['address'];
+        $this->city = $dbRow['city'];
+        $this->zip = $dbRow['zip'];
+        $this->price = $dbRow['price'];
+        $this->rooms = $dbRow['rooms'];
+        $this->bathrooms = $dbRow['bathrooms'];
+        $this->description = $dbRow['description'];
+        $this->when_added = $dbRow['when_added'];
+        $this->when_modified = $dbRow['when_modified'];
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getAddress()
+    {
+        return $this->address;
+    }
+    public function getCity()
+    {
+        return $this->city;
+    }
+    public function getZip()
+    {
+        return $this->zip;
+    }
+    public function getPrice()
+    {
+        return $this->price;
+    }
+    public function getDateAdded()
+    {
+        return $this->when_added;
+    }
+    public function getDateModified()
+    {
+        return $this->when_modified;
+    }
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    public function getBathrooms()
+    {
+        return $this->bathrooms;
+    }
+    public function getRooms()
+    {
+        return $this->rooms;
+    }
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
--- M2/public_html/pdotest.php
+++ M2/public_html/pdotest.php
@@ -4,6 +4,8 @@ require ("Database.php");
 // $dsn = 'mysql:host=localhost;dbname=student_f14g03;port=8889';
  $db = new Database();
 // $db = new PDO($dsn, 'f14g03','fzR-5NY-5oM-W2y');
+ 
+ //SAMPLE USING UserData
  $dataSet = $db->getUsers();
  if($dataSet)
  {
@@ -20,6 +22,31 @@ require ("Database.php");
  }
  else
      echo "No Results Found!";
+ 
+ //SAMPLE USING ListingData
+ $listingSet = $db->searchListings('94324');
+ if($listingSet)
+ {
+     foreach($listingSet as $listingData)
+     {
+         echo "<p>";
+         echo "Id: ".$listingData->getId();
+         echo "Address: ".$listingData->getAddress();
+         echo "City: ".$listingData->getCity();
+         echo "Zip: ".$listingData->getZip();
+         echo "Price: ".$listingData->getPrice();
+         echo "Added: ".$listingData->getDateAdded();
+         echo "Modified: ".$listingData->getDateModified();
+         echo "Description: ".$listingData->getDescription();
+         echo "Bathrooms: ".$listingData->getBathrooms();
+         echo "Rooms: ".$listingData->getRooms();   
+     }
+ }
+ else
+     echo "No Results Found!";
+ 
+ 
+ 
  ?>
 <!DOCTYPE html>
 <html>
