9fc8da53eca394dd64db6bb045e217f833e15a05
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-10-26 06:38:01 +0000

    Completed Php O O P - codename POOP

--- M2/public_html/Database.php
+++ M2/public_html/Database.php
@@ -44,10 +44,10 @@ class Database
             :username, :password, :type, :email)";
                                           
         $stmt = $this->con->prepare($sql);
-        $stmt->bindParam(':username', $input['username'], PDO::PARAM_STR);       
-        $stmt->bindParam(':password', $input['password'], PDO::PARAM_STR); 
-        $stmt->bindParam(':type', $input['type'], PDO::PARAM_STR); 
-        $stmt->bindParam(':email', $input['email'], PDO::PARAM_STR);   
+        $stmt->bindParam(':username', $input->getUserName(), PDO::PARAM_STR);       
+        $stmt->bindParam(':password', $input->getUserPassword(), PDO::PARAM_STR); 
+        $stmt->bindParam(':type', $input->getUserType(), PDO::PARAM_STR); 
+        $stmt->bindParam(':email', $input->getUserEmail(), PDO::PARAM_STR);   
         
         $stmt->execute();  
     }
@@ -62,7 +62,10 @@ class Database
         $sql = "SELECT * from houses WHERE city = '$input' OR zip = '$input'";
         foreach ($this->con->query($sql) as $row) 
         {
-            $dataSet[] = new ListingData($row);
+            $imgstack = $this->getImages($row['id']);
+            $newListing = new ListingData($row);
+            $newListing->setImages($imgstack);
+            $dataSet[] = $newListing;
         }
         if (!empty($dataSet))
             return $dataSet;
@@ -71,30 +74,83 @@ class Database
     }
  
     /**
+     * Get Realtor's Listings from the Database
+     * @return \UserData
+     */
+    public function getListings($realtorId)
+    {
+        $imgstack = array();
+        $sql = "SELECT * from houses WHERE userid = '$realtorId'";
+        foreach ($this->con->query($sql) as $row) 
+        {
+            $imgstack = $this->getImages($row['id']);
+            $newListing = new ListingData($row);
+            $newListing->setImages($imgstack);
+            $dataSet[] = $newListing;
+            
+        }
+        if (!empty($dataSet))
+            return $dataSet;
+        else
+            return null;
+    }
+    
+    /**
      * Add a Listing to the Database
      * @param type $input
      */
     public function addListing($input)
     {
         $sql = "INSERT INTO houses(address, city, zip, price, rooms, bathrooms, 
-            description, when_added, when_modified) VALUES (
+            description, userid, when_added, when_modified) VALUES (
             :address, :city, :zip, :price, :rooms, :bathrooms, 
-            :description, :when_added, :when_modified)";
-                                          
+            :description, :userid, :when_added, :when_modified)";
+                                       
         $stmt = $this->con->prepare($sql);
-        $stmt->bindParam(':address', $input['address'], PDO::PARAM_STR);       
-        $stmt->bindParam(':city', $input['city'], PDO::PARAM_STR); 
-        $stmt->bindParam(':zip', $input['zip'], PDO::PARAM_INT);  
-        $stmt->bindParam(':price', $input['price'], PDO::PARAM_INT); 
-        $stmt->bindParam(':rooms', $input['rooms'], PDO::PARAM_INT);   
-        $stmt->bindParam(':bathrooms', $input['bathrooms'], PDO::PARAM_INT); 
-        $stmt->bindParam(':description', $input['description'], PDO::PARAM_STR);   
-        $stmt->bindParam(':when_added', $input['when_added'], PDO::PARAM_STR); 
-        $stmt->bindParam(':when_modified', $input['when_modified'], PDO::PARAM_STR);   
-
+        $stmt->bindParam(':address', $input->getAddress(), PDO::PARAM_STR);       
+        $stmt->bindParam(':city', $input->getCity(), PDO::PARAM_STR); 
+        $stmt->bindParam(':zip', $input->getZip(), PDO::PARAM_INT);  
+        $stmt->bindParam(':price', $input->getPrice(), PDO::PARAM_INT); 
+        $stmt->bindParam(':rooms', $input->getRooms(), PDO::PARAM_INT);   
+        $stmt->bindParam(':bathrooms', $input->getBathrooms(), PDO::PARAM_INT); 
+        $stmt->bindParam(':description', $input->getDescription(), PDO::PARAM_STR);   
+        $stmt->bindParam(':userid', $input->getUserId(), PDO::PARAM_STR);   
+        $stmt->bindParam(':when_added', date("Y/m/d"), PDO::PARAM_STR); 
+        $stmt->bindParam(':when_modified', date("Y/m/d"), PDO::PARAM_STR);   
+  
         $stmt->execute();       
     }
 
+    /**
+     * Edit a Listing in the Database
+     * @param type $input
+     */
+    public function editListing($input)
+    {
+        $sql = "UPDATE houses SET address = :address, 
+            city = :city, 
+            zip = :zip, 
+            price = :price, 
+            rooms = :rooms, 
+            bathrooms = :bathrooms, 
+            description = :description,  
+            when_modified = :when_modified
+            WHERE id = :id";
+                                          
+        $stmt = $this->con->prepare($sql);
+        $stmt->bindParam(':address', $input->getAddress(), PDO::PARAM_STR);       
+        $stmt->bindParam(':city', $input->getCity(), PDO::PARAM_STR); 
+        $stmt->bindParam(':zip', $input->getZip(), PDO::PARAM_INT);  
+        $stmt->bindParam(':price', $input->getPrice(), PDO::PARAM_INT); 
+        $stmt->bindParam(':rooms', $input->getRooms(), PDO::PARAM_INT);   
+        $stmt->bindParam(':bathrooms', $input->getBathrooms(), PDO::PARAM_INT); 
+        $stmt->bindParam(':description', $input->getDescription(), PDO::PARAM_STR);   
+        $stmt->bindParam(':when_modified', date("Y/m/d"), PDO::PARAM_STR);   
+        $stmt->bindParam(':id', $input->getId(), PDO::PARAM_INT);   
+
+        $stmt->execute();       
+    }
+    
     /**
      * Delete a Listing from the database
      * @param type $id
@@ -106,6 +162,24 @@ class Database
         //$stmt->bindParam(':id', $id, PDO::PARAM_STR, 12);   
         $stmt->bindParam(':id', $id, PDO::PARAM_INT);   
         $stmt->execute();
+    }
+    
+    /**
+     * Get images associated with a listing
+     * @param type $listingId
+     * @return imgStack
+     */
+    private function getImages($listingId)
+    {
+        $sql2 = "SELECT * from images WHERE houseid = '$listingId'";
+        foreach ($this->con->query($sql2) as $row2) 
+        {
+            $imgstack[] = $row2['path'];
+        }
+        if (!empty($imgstack))
+            return $imgstack;
+        else
+            return null;
     }
 }
 ?>
--- M2/public_html/pdotest.php
+++ M2/public_html/pdotest.php
@@ -1,76 +1,105 @@
 <?php
 require ("Database.php");
-// $dsn = 'mysql:host=localhost;dbname=student_f14g03';
-// $dsn = 'mysql:host=localhost;dbname=student_f14g03;port=8889';
- $db = new Database();
-// $db = new PDO($dsn, 'f14g03','fzR-5NY-5oM-W2y');
- 
+$db = new Database();
+
  /**
   * UserData SAMPLE
   */
- //Get Userinfo
- $dataSet = $db->getUsers();
- if($dataSet)
- {
-     foreach($dataSet as $data)
-     {
-         echo "<p>";
-         echo "UserName: ".$data->getUserName();
-         echo "PassWord: ".$data->getUserPassword();
-         echo "UserID: ".$data->getUserId();
-         echo "UserType: ".$data->getUserType();
-         
-         echo "</p>";
-     }
- }
- else
-     echo "No Results Found!";
- //ADD
-// $registration['username'] = "aaaaaaaa";
-// $registration['password'] = "aaaaaaaas";
-// $registration['type'] = "customer";
-// $registration['email'] = "aaaaaaasajdoas@asmdoann.com";
-// $db->addUser($registration);
- 
-  /**
-  * ListingData SAMPLE
-  */
- //DELETE
- //$db->deleteListing(14);
- //ADD
-// $input['address'] = "3571 Culver Trail";
+ //GET USERS
+echo '<p>GET</p>';
+$dataSet = $db->getUsers();
+foreach($dataSet as $data)
+{
+    echo "<p>";
+    echo "UserName: ".$data->getUserName();
+    echo "PassWord: ".$data->getUserPassword();
+    echo "UserID: ".$data->getUserId();
+    echo "UserType: ".$data->getUserType();
+    echo "</p>";
+}
+//ADD USER
+echo '<p>ADD</p>';
+$registration['username'] = "Jacha"; 
+$registration['password'] = "ahhahaha";
+$registration['type'] = "customer";
+$registration['email'] = "ohno@asmdoann.com";
+$newUser = new UserData($registration);
+$db->addUser($newUser);
+
+ /**
+ * ListingData SAMPLE
+ */
+echo '<p>LISTINGS</p>';
+//DELETE
+echo '<p>DELETE</p>';
+$db->deleteListing(24);
+//ADD
+// echo '<p>ADD</p>';
+// $input['address'] = "2345 Culver Trail";
 // $input['city'] = "Faribault";
-// $input['zip'] = "94112";
-// $input['price'] = "5";
+// $input['zip'] = "94132";
+// $input['price'] = "15";
 // $input['rooms'] = "5";
 // $input['bathrooms'] = "3.5";
 // $input['description'] = "Cozy home away from home";
-// $input['when_added'] = "today";
-// $input['when_modified'] = "tomorrow";
-// $db->addListing($input);
- //SEARCH
- //$listingSet = $db->searchListings('94132');
- //$listingSet = $db->searchListings('San Francisco');
- if($listingSet)
- {
-     foreach($listingSet as $listingData)
-     {
-         echo "<p>";
-         echo "Id: ".$listingData->getId();
-         echo "Address: ".$listingData->getAddress();
-         echo "City: ".$listingData->getCity();
-         echo "Zip: ".$listingData->getZip();
-         echo "Price: ".$listingData->getPrice();
-         echo "Added: ".$listingData->getDateAdded();
-         echo "Modified: ".$listingData->getDateModified();
-         echo "Description: ".$listingData->getDescription();
-         echo "Bathrooms: ".$listingData->getBathrooms();
-         echo "Rooms: ".$listingData->getRooms();   
-     }
- }
- else
-     echo "No Results Found!";
- 
+// $input['userid'] = "5";
+// $newListing = new ListingData($input);
+// $db->addListing($newListing);
+//EDIT
+echo '<p>EDIT</p>';
+$input['address'] = "3571 Culver Trail";
+$input['city'] = "Far";
+$input['zip'] = "94132";
+$input['price'] = "5";
+$input['rooms'] = "10";
+$input['bathrooms'] = "3.5";
+$input['description'] = "Cozzzzy home away from home";
+$input['id'] = "7";
+$input['userid'] = "6";
+$newListing = new ListingData($input);
+$db->editListing($newListing);
+//SEARCH
+echo '<p>SEARCH</p>';
+$listingSet = $db->searchListings('94132');
+//$listingSet = $db->searchListings('San Francisco');
+foreach((array)$listingSet as $listingData)
+{
+    echo "<p>";
+    echo "Id: ".$listingData->getId();
+    echo "Address: ".$listingData->getAddress();
+    echo "City: ".$listingData->getCity();
+    echo "Zip: ".$listingData->getZip();
+    echo "Price: ".$listingData->getPrice();
+    echo "Added: ".$listingData->getDateAdded();
+    echo "Modified: ".$listingData->getDateModified();
+    echo "Description: ".$listingData->getDescription();
+    echo "Bathrooms: ".$listingData->getBathrooms();
+    echo "Rooms: ".$listingData->getRooms(); 
+    echo "</p>";
+}
+//REALTOR
+echo '<p>REALTOR</p>';
+$listingSet = $db->getListings('3');
+//$listingSet = $db->searchListings('San Francisco');
+foreach((array)$listingSet as $listingData)
+{
+    echo "<p>";
+    echo "Id: ".$listingData->getId();
+    echo "Address: ".$listingData->getAddress();
+    echo "City: ".$listingData->getCity();
+    echo "Zip: ".$listingData->getZip();
+    echo "Price: ".$listingData->getPrice();
+    echo "Added: ".$listingData->getDateAdded();
+    echo "Modified: ".$listingData->getDateModified();
+    echo "Description: ".$listingData->getDescription();
+    echo "Bathrooms: ".$listingData->getBathrooms();
+    echo "Rooms: ".$listingData->getRooms(); 
+    foreach((array)$listingData->getImages() as $img)
+    {
+       echo "Image: ".$img;
+    }
+    echo "</p>";
+}
  
  
  ?>
