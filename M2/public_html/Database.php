c2369376f153b64b5d15f4b7b21e1b9ea7451344
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-10-26 01:00:43 +0000

    working on database

--- M2/public_html/Database.php
+++ M2/public_html/Database.php
@@ -5,7 +5,9 @@ class Database
 {
     private $con;
     
-    //Construct a new database object
+    /**
+     * Constructor for the databse object
+     */
     public function __construct()
     {
         $database = 'student_f14g03';
@@ -15,7 +17,10 @@ class Database
         $this->con = new PDO("mysql:host=".$host.";dbname=".$database,$username,$password);
     }
     
-    //Get Users from the database
+    /**
+     * Get Users from the Database
+     * @return \UserData
+     */
     public function getUsers()
     {
         $sql = "SELECT * from usertable";
@@ -29,7 +34,29 @@ class Database
             return null;
     }
     
-    //Get housese from the database
+    /**
+     * Add new User:Customer
+     * @param type $input
+     */
+    public function addUser($input)
+    {
+        $sql = "INSERT INTO usertable(username, password, type, email) VALUES (
+            :username, :password, :type, :email)";
+                                          
+        $stmt = $this->con->prepare($sql);
+        $stmt->bindParam(':username', $input['username'], PDO::PARAM_STR);       
+        $stmt->bindParam(':password', $input['password'], PDO::PARAM_STR); 
+        $stmt->bindParam(':type', $input['type'], PDO::PARAM_STR); 
+        $stmt->bindParam(':email', $input['email'], PDO::PARAM_STR);   
+        
+        $stmt->execute();  
+    }
+    
+    /**
+     * Search Listings
+     * @param type $input
+     * @return \ListingData
+     */
     public function searchListings($input)
     {
         $sql = "SELECT * from houses WHERE city = '$input' OR zip = '$input'";
@@ -42,7 +69,44 @@ class Database
         else
             return null;
     }
-    
+ 
+    /**
+     * Add a Listing to the Database
+     * @param type $input
+     */
+    public function addListing($input)
+    {
+        $sql = "INSERT INTO houses(address, city, zip, price, rooms, bathrooms, 
+            description, when_added, when_modified) VALUES (
+            :address, :city, :zip, :price, :rooms, :bathrooms, 
+            :description, :when_added, :when_modified)";
+                                          
+        $stmt = $this->con->prepare($sql);
+        $stmt->bindParam(':address', $input['address'], PDO::PARAM_STR);       
+        $stmt->bindParam(':city', $input['city'], PDO::PARAM_STR); 
+        $stmt->bindParam(':zip', $input['zip'], PDO::PARAM_INT);  
+        $stmt->bindParam(':price', $input['price'], PDO::PARAM_INT); 
+        $stmt->bindParam(':rooms', $input['rooms'], PDO::PARAM_INT);   
+        $stmt->bindParam(':bathrooms', $input['bathrooms'], PDO::PARAM_INT); 
+        $stmt->bindParam(':description', $input['description'], PDO::PARAM_STR);   
+        $stmt->bindParam(':when_added', $input['when_added'], PDO::PARAM_STR); 
+        $stmt->bindParam(':when_modified', $input['when_modified'], PDO::PARAM_STR);   
+
+        $stmt->execute();       
+    }
+
+    /**
+     * Delete a Listing from the database
+     * @param type $id
+     */
+    public function deleteListing($id)
+    {
+        $sql = "DELETE FROM houses WHERE id =  :id";
+        $stmt = $this->con->prepare($sql);
+        //$stmt->bindParam(':id', $id, PDO::PARAM_STR, 12);   
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);   
+        $stmt->execute();
+    }
 }
 ?>
 
--- M2/public_html/pdotest.php
+++ M2/public_html/pdotest.php
@@ -5,7 +5,10 @@ require ("Database.php");
  $db = new Database();
 // $db = new PDO($dsn, 'f14g03','fzR-5NY-5oM-W2y');
  
- //SAMPLE USING UserData
+ /**
+  * UserData SAMPLE
+  */
+ //Get Userinfo
  $dataSet = $db->getUsers();
  if($dataSet)
  {
@@ -22,9 +25,32 @@ require ("Database.php");
  }
  else
      echo "No Results Found!";
+ //ADD
+// $registration['username'] = "aaaaaaaa";
+// $registration['password'] = "aaaaaaaas";
+// $registration['type'] = "customer";
+// $registration['email'] = "aaaaaaasajdoas@asmdoann.com";
+// $db->addUser($registration);
  
- //SAMPLE USING ListingData
- $listingSet = $db->searchListings('94324');
+  /**
+  * ListingData SAMPLE
+  */
+ //DELETE
+ //$db->deleteListing(14);
+ //ADD
+// $input['address'] = "3571 Culver Trail";
+// $input['city'] = "Faribault";
+// $input['zip'] = "94112";
+// $input['price'] = "5";
+// $input['rooms'] = "5";
+// $input['bathrooms'] = "3.5";
+// $input['description'] = "Cozy home away from home";
+// $input['when_added'] = "today";
+// $input['when_modified'] = "tomorrow";
+// $db->addListing($input);
+ //SEARCH
+ //$listingSet = $db->searchListings('94132');
+ //$listingSet = $db->searchListings('San Francisco');
  if($listingSet)
  {
      foreach($listingSet as $listingData)
