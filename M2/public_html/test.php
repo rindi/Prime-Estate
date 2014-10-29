61e608c6c7783ab253ff49077b3e39dd9e3e7591
rindi <rindi@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-10-29 17:52:11 +0000

    Changes to test file for Maps

--- M2/public_html/test.php
+++ M2/public_html/test.php
@@ -10,6 +10,7 @@ $city = 'San Francisco';
 $addrns = preg_replace('/\s+/', '+', $addr);
 echo $addrns;
 $cityns = preg_replace('/\s+/', '+', $city);
-$map = 'https://maps.googleapis.com/maps/api/staticmap?center=/'.addrns.','.cityns.'&zoom=14&size=400x400&markers=color:blue7Clabel:S%7C11206'.addrns.','.cityns;
+echo $cityns;
+$map = 'https://maps.googleapis.com/maps/api/staticmap?center=/'.$addrns.','.$cityns.'&zoom=14&size=400x400&markers=color:blue7Clabel:S%7C11206'.$addrns.','.$cityns;
 echo $map;
 ?>
