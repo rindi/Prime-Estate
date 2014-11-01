659fe83324577959c50aefa1c91e241fe5c0c410
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-01 23:39:06 +0000

    working on upload feature

--- m3/controllers/controller.php
+++ m3/controllers/controller.php
@@ -10,10 +10,13 @@ class Controller
         $db_username = 'f14g03';
         $db_password = 'fzR-5NY-5oM-W2y';
         $db_host = 'localhost';
-        $db_connect = new PDO("mysql:host=".$db_host.";dbname=".$db_name, $db_username, $db_password);
+        $this->db_connect = new PDO("mysql:host=".$db_host.";dbname=".$db_name, $db_username, $db_password);
         
         /* test connection */
-        //if( $db_connect == true ) echo "CONNECTED TO DB JUST FINE. first one";
+        if( $this->db_connect == true ) 
+        {
+            echo "number 1 working!";
+        }
     }
 }
 
