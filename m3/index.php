e73acb4ee040a69c45ceeece76f4f87a0f15c993
rindi <rindi@users.noreply.github.com>
2014-11-01 21:50:13 +0000

    updated index.php

--- m3/index.php
+++ m3/index.php
@@ -7,4 +7,19 @@
  */
 
 ?>
-hellono
+<html>
+    <head>
+        <title> Prime Estate </title>
+    </head>
+    <body>
+        <h1 align="center">
+            Welcome to Prime Estate.
+        </h1>
+        <form style="text-align:center; margin: 0px auto" action="searchresults.php" method="POST">
+        <input type="search" name="searchvalue">
+        <input type="submit" class="btn btn-inverse" value="Find home">
+        <!--<span class="glyphicon glyphicon-search"></span>-->
+        </input>
+        </form>
+    </body>
+</html>
