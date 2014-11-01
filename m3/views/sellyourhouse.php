f01da7b2fba3afea989023b099b4bc0c83082f53
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-01 22:14:10 +0000

    modifying filess

---
+++ m3/views/sellyourhouse.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<html>
+    <head>
+        <title> Sell Your House! </title>
+    </head>
+    <body>
+        <h1 align="center">
+            Welcome to Prime Estate.
+        </h1>
+        <form style="text-align:center; margin: 0px auto" action="searchresults.php" method="POST">
+        <input type="search" name="searchvalue">
+        <input type="submit" class="btn btn-inverse" value="Find home">
+        <!--<span class="glyphicon glyphicon-search"></span>-->
+        </input>
+        </form>
+    </body>
+</html>
