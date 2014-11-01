f091731f029a672a73d61a524f9f4d07ba6f62e8
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-01 22:35:45 +0000

    modifying filess

--- m3/views/sellyourhouse.php
+++ m3/views/sellyourhouse.php
@@ -9,15 +9,13 @@
 ?>
 <html>
     <head>
-        <title> Sell Your House! </title>
+        <title> Sell Your House With PRIME ESTATE </title>
     </head>
     <body>
         <h1 align="center">
-            Welcome to Prime Estate.
+            Sell your house with Prime Estate today!
         </h1>
-        <form style="text-align:center; margin: 0px auto" action="searchresults.php" method="POST">
-        <input type="search" name="searchvalue">
-        <input type="submit" class="btn btn-inverse" value="Find home">
+        
         <!--<span class="glyphicon glyphicon-search"></span>-->
         </input>
         </form>
---
+++ m3/views/uploaditnow.php
@@ -0,0 +1,42 @@
+
+<html>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        Please choose a file: <input type="file" name="uploadFile"><br>
+        <input type="submit" value="Upload File">
+    </form>
+</html>
+<?php
+$target_dir = "assets/";
+$target_dir = $target_dir . basename( $_FILES["uploadFile"]["name"]);
+$uploadOk=1;
+
+// Check if file already exists
+if (file_exists($target_dir . $_FILES["uploadFile"]["name"])) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+// Check file size
+if ($uploadFile_size > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+
+// Only GIF files allowed 
+if (!($uploadFile_type == "image/gif")) {
+    echo "Sorry, only GIF files are allowed.";
+    $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else { 
+    if (move_uploaded_file($_FILES["uploadFile"]["tmp_name"], $target_dir)) {
+        echo "The file ". basename( $_FILES["uploadFile"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
