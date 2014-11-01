c93e20a27769c6949a43337a60b2143577991354
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-01 21:55:53 +0000

    minor change to controllers/controller.php

--- m3/controllers/controller.php
+++ m3/controllers/controller.php
@@ -10,13 +10,10 @@ class Controller
         $db_username = 'f14g03';
         $db_password = 'fzR-5NY-5oM-W2y';
         $db_host = 'localhost';
-        $this->db_connect = new PDO("mysql:host=".$db_host.";dbname=".$db_name, $db_username, $db_password);
+        $db_connect = new PDO("mysql:host=".$db_host.";dbname=".$db_name, $db_username, $db_password);
         
         /* test connection */
-        if( $db_connect )
-        {
-            echo "CONNECTED TO DB JUST FINE.";
-        }
+        if( $db_connect ) echo "CONNECTED TO DB JUST FINE.";
     }
 }
 
