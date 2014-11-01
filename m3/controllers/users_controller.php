effddf485a03020344428a52d580f0100ea8df0d
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-01 22:34:24 +0000

    Connect to db from mvc_test.php -> users_controller.php which inherits connection from controller.php

--- m3/controllers/users_controller.php
+++ m3/controllers/users_controller.php
@@ -1,16 +1,10 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+require ("../controllers/controller.php");
 class UsersController extends Controller
 {
     
     public function __construct() {
-        super();
+        parent::__construct();
         if( $db_connect ) echo "CONNECTED TO DB JUST FINE.";
     }
 }
