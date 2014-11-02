80d94bcfa4f69b64047ced34370099aa5b357031
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-02 03:43:31 +0000

    working on upload feature

--- m3/views/upload.php
+++ m3/views/upload.php
@@ -1,4 +1,5 @@
 <?php
+//houseid_nameofimg_timestampofupload.jpg
 $target_dir = "assets/images/";
 $target_dir = $target_dir . basename( $_FILES["uploadFile"]["name"]);
 $uploadOk=1;
@@ -6,38 +7,51 @@ $uploadOk=1;
 $suffix = substr($_FILES["uploadFile"]["name"], strpos($_FILES["uploadFile"]["name"], ".") + 1);
 
 // Check if file already exists
-if (file_exists($target_dir . $_FILES["uploadFile"]["name"])) {
+if (file_exists($target_dir . $_FILES["uploadFile"]["name"])) 
+{
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
 
-
 // Check file size
-//if ($uploadFile_size > 500000) {
-if ($_FILES["uploadFile"]["size"] > 500000) {
+if ($_FILES["uploadFile"]["size"] > 500000) 
+{
+    echo $_FILES["uploadFile"]["size"];
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
 
-//// Only GIF files allowed 
-//if (!($uploadFile_type == "image/gif")) {
-//    echo "Sorry, only GIF files are allowed.";
-//    $uploadOk = 0;
-//}
 // Only GIF files allowed 
-if (!(strcasecmp($suffix, 'gif') == 0)) {
-    echo "Sorry, only GIF files are allowed.";
+if (($_FILES["uploadFile"]["type"] == "image/gif") || ($_FILES["uploadFile"]["type"] == "image/jpeg")) 
+{
+    pass;
+}
+else
+{
+    echo "404!";
     $uploadOk = 0;
 }
+//elseif (!($_FILES["uploadFile"]["type"] == "image/jpeg"))
+//{
+//    echo "Sorry, only JPEG files are allowed.";
+//    $uploadOk = 0;
+//}
+
 
 // Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
+if ($uploadOk == 0) 
+{
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
-} else { 
-    if (move_uploaded_file($_FILES["uploadFile"]["tmp_name"], $target_dir)) {
+} 
+else 
+{ 
+    if (move_uploaded_file($_FILES["uploadFile"]["tmp_name"], $target_dir)) 
+    {
         echo "The file ". basename( $_FILES["uploadFile"]["name"]). " has been uploaded.";
-    } else {
+    } 
+    else 
+    {
         echo "Sorry, there was an error uploading your file.";
     }
 }
@@ -50,7 +64,7 @@ echo $now;
 
 <body>
  <h2>Your image:</h2>
- <img src="<?php echo $now;?>" alt="Mountain View" style="width:304px;height:228px">
+ <img src="<?php echo $now;?>" alt="img" style="width:304px;height:228px">
 </body>
 
 </html>
