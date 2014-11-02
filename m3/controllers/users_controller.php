b7600b661343464414a32c2e7ef31c7f26b7b258
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-02 01:25:05 +0000

    code to test adding user.

--- m3/controllers/users_controller.php
+++ m3/controllers/users_controller.php
@@ -15,9 +15,31 @@ class UsersController extends Controller
         $sql = "SELECT * from usertable";
         foreach( parent::$this->db_connect->query($sql) as $row )
             $dataSet[] = new user_model($row);
-        if( !empty($dataSet) )
-            return $dataSet;
-        else
-            return null;
+        
+        print_r($dataSet[0]);
+        
+        $dataSet[1]['username'] = "a";
+        $dataSet[1]['password'] = "b";
+        $dataSet[1]['email'] = "c";
+        $dataSet[1]['type'] = "d";
+        print_r($dataSet[1]);
     }
+    
+    public function addUser($input)
+    {
+        $sql = "INSERT INTO 
+                usertable (username, password, type, email) 
+                VALUES (:username, :password, :type, :email)";
+        
+        $q = $this->db_connect->prepare($sql);
+        $q->execute(array
+                        (':username'    => $input->getUserName(),
+                         ':password'    => $input->getUserPassword(),
+                         ':type'        => $input->getUserType(),
+                         ':email'       => $input->getUserEmail() 
+                        )
+                    );
+        
+    }
+    
 }
--- m3/views/mvc_testing.php
+++ m3/views/mvc_testing.php
@@ -6,4 +6,14 @@ $user_contr = new UsersController();
 $users = $user_contr->getUsers();
 print_r($user_contr);
 echo "<br>";
-print_r($users);
+//print_r($users);
+
+foreach($users as $user)
+{
+    print_r($user);
+}
+
+//test user creation
+$test_add_user = new Model();
+$temp = $test_add_user->testAddUser();
+$user_contr->addUser($temp);
