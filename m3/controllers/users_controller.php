56b25bb2315e2e16da12e87f4890cf4a1cb3b978
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-02 00:42:52 +0000

    Connect to db works like a charm. Small update.

--- m3/controllers/users_controller.php
+++ m3/controllers/users_controller.php
@@ -1,12 +1,23 @@
 <?php
 require ("../controllers/controller.php");
+require ("../models/user_model.php");
 class UsersController extends Controller
 {
     public function __construct( ) 
     {
         parent::__construct();
-        
         /* test if connected to db */
         // if( parent::$this->db_connect ) echo "this is finally working";
+    }
+    
+    public function getUsers()
+    {   
+        $sql = "SELECT * from usertable";
+        foreach( parent::$this->db_connect->query($sql) as $row )
+            $dataSet[] = new user_model($row);
+        if( !empty($dataSet) )
+            return $dataSet;
+        else
+            return null;
     }
 }
