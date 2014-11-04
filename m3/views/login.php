41d546932b8314b6e23ca6c3c6302d01a7a9dd67
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-04 06:19:53 +0000

    upload cleaning

--- m3/views/login.php
+++ m3/views/login.php
@@ -4,6 +4,9 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<?php
+include 'navbar.php';
+?>
 <html>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <head>
@@ -11,19 +14,6 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-    <nav class="navbar navbar-default" role="navigation">
-        <div class="navbar-header">
-        <a class="navbar-brand" href="index.php">Prime Estate</a>
-        <ul class="nav navbar-nav">
-        <li class="active">
-             <a href="login.php">Sign in</a>
-         </li>
-         <li class="active">
-             <a href="registration.php">Register</a>
-         </li>    
-        </div>
-    </nav>
-        <h2 style="text-align:center; margin: 0px auto"> Sign in to your account </h2> <br/><br/>
         <form style="text-align:center; margin: 0px auto" action="Brain/login.php" method="POST">
             <input type="text" name="username" placeholder="Username">
             <input type="password" name="password" placeholder="Password">
