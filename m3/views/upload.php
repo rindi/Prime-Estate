45986ca59798cf23d12ba9e3072f2390e14006e4
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-04 03:57:17 +0000

    upload cleaning

--- m3/views/upload.php
+++ m3/views/upload.php
@@ -1,5 +1,5 @@
 <?php
-
+include 'navbar.php';
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,14 +12,14 @@
         <title> Upload Your image </title>
     </head>
     <body>
-        <h1 align="center">
-            Please select the image you wish to upload.
-        </h1>
+        <h3>
+            Please select the image you wish to upload
+        </h3>
         
         <!--<span class="glyphicon glyphicon-search"></span>-->
         </input>
         </form>
-        <form align="center" action="upload.php" method="post" enctype="multipart/form-data">
+        <form action="uploadcomplete.php" method="post" enctype="multipart/form-data">
             Please choose a file: <input type="file" name="uploadFile"><br>
             <input type="submit" value="Upload File">
         </form>
