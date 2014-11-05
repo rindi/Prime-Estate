5a34d8d8991d3df41b13a5cdb943eb3912abe641
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-05 07:44:53 +0000

    Instantiate current listing.

--- m3/views/listing_page.php
+++ m3/views/listing_page.php
@@ -1,7 +1,12 @@
 <?php
 
 include("navbar.php");
-
+$listing_controller = new listings_controller();
+$current_listing = $list_controller->getLisint($_GET['id']);
 ?>
 
-<h1>Edit house number <?php echo $_GET['id']; ?></h1>
+<h1>Show house #<?php echo $current_listing; ?></h1>
+
+<div id="listing">
+    
+</div>
