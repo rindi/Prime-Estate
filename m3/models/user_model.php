4d7b22fdeec5580451427e00cce4c8ea03e72489
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-05 21:48:51 +0000

    crisis averted!

--- m3/models/user_model.php
+++ m3/models/user_model.php
@@ -10,24 +10,16 @@ class user_model
     
     public function __construct($dbRow)
     { 
-        if( !$dbRow )
+
+        if (count($dbRow)>4)
         {
-            $this->username = $dbRow['username'];
-            $this->password = $dbRow['password'];
-            $this->email = $dbRow['email'];
-            $this->type = $dbRow['type'];
+            $this->userid = $dbRow['userid'];
         }
-        else
-        {
-            if (count($dbRow)>4)
-            {
-                $this->userid = $dbRow['userid'];
-            }
-            $this->username = $dbRow['username'];
-            $this->password = $dbRow['password'];
-            $this->email = $dbRow['email'];
-            $this->type = $dbRow['type'];
-        }   
+        $this->username = $dbRow['username'];
+        $this->password = $dbRow['password'];
+        $this->email = $dbRow['email'];
+        $this->type = $dbRow['type'];
+
 
     }
     public function getUserName()
