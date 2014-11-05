5e9db9748619ad9c53d4aeb4c0168e0855387aa0
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-05 08:49:31 +0000

    "Why wont it work?" comment added.

--- m3/views/listing_page.php
+++ m3/views/listing_page.php
@@ -29,6 +29,7 @@ $listing_model = new listing_model($_GET['id']);
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-9 col-md-9 col-lg-8">
+                    <!-- WHY FOLLOWING LINE WONT WORK ? -->
                     <?php echo $listing_model->getZip(); ?>
                 </div>
             </div>
