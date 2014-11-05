320a98658f43c5ac2897348f20739e6bfeb530ed
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-05 21:15:47 +0000

    Added "Edit" link for home 41. Hopefully works again. Done.

--- m3/views/searchresults.php
+++ m3/views/searchresults.php
@@ -65,7 +65,7 @@ include 'navbar.php';
             $mapurl = $listingData->getMap();;
             echo "<tbody><tr>";
             echo "<td><a href=\"listing_page.php?id=".$houseval."\">" . $houseval . "</a>";
-            echo "<a href=\"edit_listing.php?id=41\">Edit</a>";
+            echo "<a href=\"edit_listing.php?id=".$houseval."\">Edit</a>";
             echo "</td>";
             echo "<td>" . $listingData->getAddress() . "</td>";
             echo "<td>" . $listingData->getCity() . "</td>";
