5e5022bd4333e5796856d9179926a4b53a34351a
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-05 08:35:25 +0000

    See if listing model instantiation works.

--- m3/views/listing_page.php
+++ m3/views/listing_page.php
@@ -8,6 +8,8 @@ $listing_controller = new listings_controller();
 $current_listing = $listing_controller->getListing($_GET['id']);
 $images = $listing_controller->getImages($_GET['id']);
 $image_1 = $images[0];
+
+$listing_model = new listing_model($_GET['id']);
 ?>
 
 <div class="container">
@@ -17,18 +19,17 @@ $image_1 = $images[0];
         </div>
         <div class="panel-body">
             <div class="row">
-                <div class="col-xs-2 col-sm-3 col-md-3 col-lg-4">
+                <div class="col-xs-12 col-sm-3 col-md-3 col-lg-4">
                     <div class="row">
-                        <div class="col-xs-6 col-md-3">
+                        <div class="col-xs-12">
                             <a href="<?php echo $images[0];?>" class="thumbnail">
                                 <img src="<?php echo $images[0];?>" alt="...">
                             </a>
                         </div>
-                        
                     </div>
                 </div>
-                <div class="col-xs-10 col-sm-9 col-md-9 col-lg-8">
-                    content 2
+                <div class="col-xs-12 col-sm-9 col-md-9 col-lg-8">
+                    
                 </div>
             </div>
         </div>
