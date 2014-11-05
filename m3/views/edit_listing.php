409e84b4229a31238cc1bfaedad409f42592964a
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-05 06:56:19 +0000

    Check if user is seller.

--- m3/views/edit_listing.php
+++ m3/views/edit_listing.php
@@ -1,3 +1,12 @@
 <?php
 
 include("navbar.php");
+
+/* check if user is seller aka. allowed to edit listing */
+if( !isset($_COOKIE['seller'] ) )
+{
+    die("Cookie 'seller' is NOT set.");
+}else
+{
+    echo "Cookie 'seller' IS not set.";
+}
