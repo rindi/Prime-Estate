d59144d73413cd22db17ec782318d6ee2dc69138
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-05 08:03:57 +0000

    Add a bit more layout for image.

--- m3/views/listing_page.php
+++ m3/views/listing_page.php
@@ -14,7 +14,21 @@ $current_listing = $listing_controller->getListing($_GET['id']);
           <h2 class="panel-title">Show house #<?php echo $current_listing['id']; ?></h2>
         </div>
         <div class="panel-body">
-          Basic panel example
+            <div class="row">
+                <div class="col-xs-2 col-sm-3 col-md-3 col-lg-4">
+                    <div class="row">
+                        <div class="col-xs-6 col-md-3">
+                            <a href="#" class="thumbnail">
+                                <img data-src="" alt="...">
+                            </a>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="col-xs-10 col-sm-9 col-md-9 col-lg-8">
+                    content 2
+                </div>
+            </div>
         </div>
     </div>
 </div>
