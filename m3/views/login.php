2db895631213f5cc2562200ec33ab56aae4b8858
rindi <rindi@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-06 03:18:29 +0000

    Initial commit for login and registration

--- m3/views/login.php
+++ m3/views/login.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 
 <?php
-include 'navbar.php';
+include 'views/navbar.php';
+
 ?>
 <html>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
@@ -10,14 +11,13 @@ include 'navbar.php';
         <title></title>
     </head>
     <body>
-        <form style="text-align:center; margin: 0px auto" action="Brain/login.php" method="POST">
-            <input type="text" name="username" placeholder="Username">
-            <input type="password" name="password" placeholder="Password">
-            <input type="submit" name="submit"> 
+        <form method="post" action="index.php" name="loginform">
+            <label for="login_username">Username</label>
+            <input id="login_username" type="text" name="login_username" placeholder="Username" required />
+            <label for="login_password">Password</label>
+            <input id="login_password" type="password" name="login_password" placeholder="Password"required />
+            <input type="submit"  name="login" value="Log in" />
         </form>
-    <footer>
-        <div class="footer navbar-fixed-bottom">
-        <a href="data_usage.php">Data usage</a>
-    </footer>
+        <a href="registration.php">Register new account</a>
     </body>
 </html>
