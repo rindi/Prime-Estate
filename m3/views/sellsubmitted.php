52ad5c12f844c03e8c84ff87485323b8c8944aed
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-08 21:10:46 +0000

    fixed profile access to user records

--- m3/views/sellsubmitted.php
+++ m3/views/sellsubmitted.php
@@ -13,9 +13,10 @@ $user = new lead_model($input);
 $lead_controller = new leads_controller();
 $lead_controller->addLead($user);
 
-echo "Thank you for contacting us ". $user->getFirstname() .", we will get in touch with you soon!";
+//echo "Thank you for contacting us ". $user->getFirstname() .", we will get in touch with you soon!";
 ?>
-
+<!--echo '<script type="text/javascript">alert("Thank you for contacting us, we will get in touch with you soon!");</script>';-->
 <script>
+type="text/javascript">alert("Thank you for contacting us, we will get in touch with you soon!");
 window.location = 'http://sfsuswe.com/~f14g03/index.php';
 </script>
