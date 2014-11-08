15bd757258c47bde2f833cc9fefb841884dab535
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-08 22:11:46 +0000

    working on admin

--- m3/views/admin.php
+++ m3/views/admin.php
@@ -1,9 +1,17 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-?> Admin home page
+include 'navbar.php';
+?>
+<html>
+    <head>
+        <title> Administrator </title>
+    </head>
+    <body>
+        <h1 align="center">
+            Administrator Functions
+        </h1>
+        
+    </body>
+</html>
+<?php
+include 'views/data_usage.php';
+?>
