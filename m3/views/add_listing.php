8c99c82e0cafc6e79b86e3c46cd736a355fdb288
rindi <rindi@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-09 00:41:44 +0000

    Set fields to required

--- m3/views/add_listing.php
+++ m3/views/add_listing.php
@@ -7,11 +7,12 @@ require '../controllers/listings_controller.php';
 ?>
 <div style="text-align:center" >
         <h3>Adding new listing:</h3>
+        <h3>Step 1.</h3>
         <form action="add_listings.php" method="post">
             <table style="text-align:center; margin: 0px auto">
-            <tr><td>Address:</td><td><input type="text" name="address" placeholder="Address" /></td>
-            <tr><td>City:</td><td><input type="text" name="city" placeholder="City" /></td>
-            <tr><td>Zip:</td><td><input type="text" name="zip" placeholder="Zip" /></td>
+            <tr><td>Address*:</td><td><input type="text" name="address" placeholder="Address" required/></td>
+            <tr><td>City*:</td><td><input type="text" name="city" placeholder="City" required/></td>
+            <tr><td>Zip*:</td><td><input type="text" name="zip" placeholder="Zip" required/></td>
             <tr><td>Price:</td><td><input type="number" name="price" placeholder="Price" min="0"/></td>
             <tr><td>Rooms #:</td><td><input type="number" name="rooms" placeholder="Rooms" min="0"/></td>
             <tr><td>Bathrooms #:</td><td><input type="number" name="bathrooms" placeholder="Bathrooms" min="0"/></td>
