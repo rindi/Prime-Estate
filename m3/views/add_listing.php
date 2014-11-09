ef5ec7be14d3a1645d2e61fec741512b2ef51ca8
rindi <rindi@users.noreply.github.com>
2014-11-09 00:45:35 +0000

    Splitting add_listing php

--- m3/views/add_listing.php
+++ m3/views/add_listing.php
@@ -3,12 +3,31 @@
 include("navbar.php");
 require '../models/listing_model.php';
 require '../controllers/listings_controller.php';
+if(isset($_POST['SubmitButton'])) 
+{
+   
+$input['address'] = $_POST['address'];
+$input['city'] = $_POST['city'];
+$input['zip'] = $_POST['zip'];
+$input['price'] = $_POST['price'];
+$input['rooms'] = $_POST['rooms'];
+$input['bathrooms'] = $_POST['bathrooms'];
+$input['description'] = $_POST['description'];
+//$input['userid'] = $_SESSION['userid'];
+$house = new listing_model($input);
+
+$listing_controller = new listings_controller();
+$listing_controller->addListing($house);
+
+echo $input['address'] . ' added';
+}
 
 ?>
 <div style="text-align:center" >
         <h3>Adding new listing:</h3>
         <h3>Step 1.</h3>
-        <form action="add_listings.php" method="post">
+        <form action="" method="post">
+        <!--<form action="add_listings.php" method="post">-->
             <table style="text-align:center; margin: 0px auto">
             <tr><td>Address*:</td><td><input type="text" name="address" placeholder="Address" required/></td>
             <tr><td>City*:</td><td><input type="text" name="city" placeholder="City" required/></td>
@@ -18,6 +37,6 @@ require '../controllers/listings_controller.php';
             <tr><td>Bathrooms #:</td><td><input type="number" name="bathrooms" placeholder="Bathrooms" min="0"/></td>
             <tr><td>Description:</td><td><textarea type="textfield" name="description"></textarea></td>
             </table>
-            <input type="submit" value="Add listing" />
+            <input type="submit" value="Add listing" name="SubmitButton"/>
         </form>
 </div>
