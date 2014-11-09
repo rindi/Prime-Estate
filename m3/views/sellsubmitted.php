9220254bdef90e07c7502e07804d55cc0eaadbc9
rindi <rindi@users.noreply.github.com>
2014-11-09 02:50:11 +0000

    Testing validity of inputs for leads

--- m3/views/sellsubmitted.php
+++ m3/views/sellsubmitted.php
@@ -28,12 +28,15 @@ $input['email'] = $email;
 $user = new lead_model($input);
 $lead_controller = new leads_controller();
 $lead_controller->addLead($user);
+echo "Thank you for contacting us ". $user->getFirstname() .", we will get in touch with you soon!";
 }
 //echo "Thank you for contacting us ". $user->getFirstname() .", we will get in touch with you soon!";
 ?>
 <!--echo '<script type="text/javascript">alert("Thank you for contacting us, we will get in touch with you soon!");</script>';-->
 <script type="text/javascript">
-    var flag = "<?php echo $flag;?>
+    var flag = "<?php echo $flag;?>";
+    document.write(flag);
+
     if(flag=='true')
     {
         alert("Thank you for contacting us, we will get in touch with you soon!");
