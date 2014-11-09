8853d2e7dd6914197537551ba32dce1b884e41d3
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-09 23:36:02 +0000

    Instantiate model & controller in update_listing.php

--- m3/views/update_listing.php
+++ m3/views/update_listing.php
@@ -12,7 +12,16 @@ $e = $_GET['rooms'];
 $f = $_GET['bathrooms'];
 $g = $_GET['description'];
 
-echo "id: ".$z;
+echo "id: ".$z."<br>"; 
+echo "address: ".$a."<br>";
+echo "city: ".$b."<br>";
+echo "zip: ".$c."<br>";
+echo "price: ".$d."<br>";
+echo "rooms: ".$e."<br>";
+echo "bathrooms: ".$f."<br>";
+echo "description: ".$g."<br>";
+
 
 $listings_controller = new listings_controller();
+$listing_model = new listing_model($z);
 ?>
