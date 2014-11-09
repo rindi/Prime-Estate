444d90ac2dd4846deeed4aba6b4ddd98f3af5f99
rindi <rindi@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-09 00:16:20 +0000

    Edited add listings.

--- m3/views/add_listings.php
+++ m3/views/add_listings.php
@@ -11,16 +11,15 @@ $input['price'] = $_POST['price'];
 $input['rooms'] = $_POST['rooms'];
 $input['bathrooms'] = $_POST['bathrooms'];
 $input['description'] = $_POST['description'];
-
+//$input['userid'] = $_SESSION['userid'];
 $house = new listing_model($input);
 
 $listing_controller = new listings_controller();
 $listing_controller->addListing($house);
 
-echo $house->getId();
-
 echo $input['address'] . ' added';
 
-header('Location: http://sfsuswe.com/~f14g03/views/listing_page.php?id=' . $house->getId());
+header('Location: http://sfsuswe.com/~f14g03/index.php');
+//header('Location: http://sfsuswe.com/~f14g03/views/listing_page.php?id=' . $house->getId());
 
 ?>
