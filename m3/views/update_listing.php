e8a2b7fd40ee18009183b9e73e3f575b29bd3854
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-09 23:43:41 +0000

    Update listing's info. (Hope it works).

--- m3/views/update_listing.php
+++ m3/views/update_listing.php
@@ -24,4 +24,14 @@ echo "description: ".$g."<br>";
 
 $listings_controller = new listings_controller();
 $listing_model = new listing_model($z);
+
+$listing_model->setAddress($a);
+$listing_model->setCity($b);
+$listing_model->setZip($c);
+$listing_model->setPrice($d);
+$listing_model->setRooms($e);
+$listing_model->setBathrooms($f);
+$listing_model->setDescription($g);
+
+$listings_controller->editListing($listing_model);
 ?>
