f869272d0be7c06f42c0b7c73ae38dc515c0b43d
rindi <rindi@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-09 03:03:30 +0000

    Added phone test

--- m3/views/sell.php
+++ m3/views/sell.php
@@ -11,8 +11,20 @@ include 'navbar.php';
         $(document).ready(function(e) {
             $('#submitlead').click(function() {
                 var sEmail = $('#email').val();
+                var sPhone = $('#phone').val();
+                if ($.trim(sPhone).length == 0) {
+                    alert('Please enter valid phone number');
+                    e.preventDefault();
+                }
                 if ($.trim(sEmail).length == 0) {
                     alert('Please enter valid email address');
+                    e.preventDefault();
+                }
+                if (validateEmail(sPhone)) {
+                   1=1;
+                }
+                else {
+                    alert('Invalid Phone Number');
                     e.preventDefault();
                 }
                 if (validateEmail(sEmail)) {
@@ -25,6 +37,15 @@ include 'navbar.php';
             });
         });
 
+        function validatePhone(phone) {
+            var filter = /^\d{10}/;
+            if (filter.test(phone)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        } 
         function validateEmail(sEmail) {
             var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
             if (filter.test(sEmail)) {
@@ -55,7 +76,7 @@ include 'navbar.php';
       <tr>
         <td>Phone Number*</td>
         <td>
-            <input type="text" name="phone" required/>
+            <input type="text" name="phone"  id="phone" maxlength="10" required/>
         </td>
       </tr>
       <tr>
