f0c7b040aacc90134a0441c211ba1aa6f966a24e
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-10 07:38:06 +0000

    Added add listings button for realtor.

--- m3/views/dashboard.php
+++ m3/views/dashboard.php
@@ -9,9 +9,10 @@ include 'navbar.php';
         <h1 align="center">
             Realtor
         </h1>
-            <a href="http://sfsuswe.com/~f14g03/views/registration.php" class="btn btn-default">Add Customer</a>
-            <a href="http://sfsuswe.com/~f14g03/views/interestedcustomers.php" class="btn btn-default">View Interested Customers</a>
-            <a href="http://sfsuswe.com/~f14g03/views/leads.php" class="btn btn-default">View Interested Sellers</a>
+        <a href="http://sfsuswe.com/~f14g03/views/registration.php" class="btn btn-default">Add Customer</a>
+        <a href="http://sfsuswe.com/~f14g03/views/interestedcustomers.php" class="btn btn-default">View Interested Customers</a>
+        <a href="http://sfsuswe.com/~f14g03/views/leads.php" class="btn btn-default">View Interested Sellers</a>
+        <a href="add_listings.php">Add listings</a>
     </body>
 </html>
 <?php
