5927e80693ff5db78f8367649386d67772cb2e92
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-10 04:44:44 +0000

    Some changes to edit_photot.php

--- m3/views/edit_photo.php
+++ m3/views/edit_photo.php
@@ -3,18 +3,22 @@ require '../controllers/listings_controller.php';
 
 $images_controller = new listings_controller();
 $all_images = $images_controller->getImages($_GET['id']);
+
+if( $_GET['removeid'] )
+{
+    echo "remove house with id ".$_GET['removeid'];
+}
 ?>
 <?php include("navbar.php"); ?>
 
 <div class="container">
     <div class="row">
         <?php for ($i = 0; $i < count($all_images); $i++): ?>
-
             <div class="col-xs-6 col-md-3">
                 <a class="thumbnail">
                     <img src="<?php echo $all_images[$i]; ?>" alt="...">
                 </a>
-                <a href="edit_photo.php?id=<?php echo $_GET['id'];?>">
+                <a href="edit_photo.php?id=<?php echo $_GET['id'];?>&removeid=2">
                     <button class="btn btn-danger" value="Remove">Remove</button>
                 </a>
             </div>
