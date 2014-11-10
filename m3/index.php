e985f1b575026dad51d10c7754ad20d2a19e56d4
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-10 23:28:55 +0000

    A little nicer search bar at the index.php

--- m3/index.php
+++ m3/index.php
@@ -9,9 +9,14 @@ include 'views/navbar.php';
         <h1 align="center">
             Welcome to Prime Estate.
         </h1>
-        <form style="text-align:center; margin: 0px auto" action="views/searchresults.php" method="POST">
-        <input type="search" name="searchvalue">
-        <input type="submit" class="btn btn-inverse" value="Find home">
+        <form class="form-horizontal" style="text-align:center; margin: 0px auto" action="views/searchresults.php" method="POST">
+            <div class="form-group">
+                <div class="col-xs-offset-3 col-xs-5">
+                  <input type="search" name="searchvalue" class="form-control  col-xs-5">
+                </div>
+                <input type="submit" class="btn btn-inverse col-xs-1" value="Find home">
+            </div>
+          
         <!--<span class="glyphicon glyphicon-search"></span>-->
         </input>
         </form>
