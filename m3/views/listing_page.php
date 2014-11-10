ebdca91ee82d1f545b7200c8968e575d11e63110
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-10 00:36:38 +0000

    Create basic non-inteructive carusel under main image.

--- m3/views/listing_page.php
+++ m3/views/listing_page.php
@@ -47,8 +47,58 @@ $mapstring = $srcstart . $enc. $mapaddress . $end;
                         <div class="col-xs-12">
                             <a href="<?php echo $images[0];?>" class="thumbnail">
                                 <img src="<?php echo $images[0];?>" alt="...">
-                                <br>this is where other stuff go.
                             </a>
+                            
+                            
+                            
+                            
+                            
+                            
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img src="<?php echo $images[1];?>" alt="First slide">
+        </div>
+        <div class="item">
+          <img src="<?php echo $images[2];?>" alt="Second slide">
+          
+        </div>
+        <div class="item">
+          <img src="<?php echo $images[3];?>" alt="Third slide">
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel -->
+                            
+                            
+    <hr>  
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
                             <iframe
                               width='340'
                               height='250'
