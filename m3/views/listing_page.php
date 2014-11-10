7f6b9ebf7243adf6d4337b06c790bfab83cf9cb7
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-10 01:12:25 +0000

    Create new file hoses_carousel.php with carousel only.

--- m3/views/listing_page.php
+++ m3/views/listing_page.php
@@ -47,37 +47,7 @@ $mapstring = $srcstart . $enc . $mapaddress . $end;
                                     <img src="<?php echo $images[0]; ?>" alt="...">
                                 </a>
                                 
-                                <!-- CARUSEL: DYNAMIC IMAGES SCROLLING -->
-                                <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                                    <!-- Indicators -->
-                                    <ol class="carousel-indicators">
-                                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                                        <?php for( $i=1; $i<count($images); $i++ ): ?>
-                                            <li data-target="#myCarousel" data-slide-to="<?php $i;?>"></li>
-                                        <?php endfor; ?>
-                                    </ol>
-                                    <div class="carousel-inner" role="listbox">
-                                        <?php foreach( $images as $image ) : ?>
-                                        <div class="item active">
-                                            <a href="<?php echo $image; ?>">
-                                                <img src="<?php echo $image; ?>" alt="First slide">
-                                            </a>
-                                        </div>
-                                        <?php endforeach; ?>
-                                        
-                                    </div>
-                                    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                                        <span class="glyphicon glyphicon-chevron-left"></span>
-                                        <span class="sr-only">Previous</span>
-                                    </a>
-                                    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                                        <span class="glyphicon glyphicon-chevron-right"></span>
-                                        <span class="sr-only">Next</span>
-                                    </a>
-                                </div><!-- /.carousel -->
-
-
-                                <hr>  
+                                <?php include("houses_carousel.php"); ?>
 
                                 <iframe
                                     width='340'
