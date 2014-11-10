038d040ad68b55a58435ecaf44a4ddccc8f23eba
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-10 23:51:57 +0000

    Improved looks of index page.

--- m3/index.php
+++ m3/index.php
@@ -6,20 +6,24 @@ include 'views/navbar.php';
         <title> Prime Estate </title>
     </head>
     <body>
-        <h1 align="center">
-            Welcome to Prime Estate.
-        </h1>
-        <form class="form-horizontal" style="text-align:center; margin: 0px auto" action="views/searchresults.php" method="POST">
-            <div class="form-group">
-                <div class="col-xs-offset-3 col-xs-5">
-                  <input type="search" name="searchvalue" class="form-control">
+        <div class=""container>
+          <div class="well well-lg col-xs-offset-1 col-xs-10">
+            <h1 align="center">
+                Welcome to Prime Estate.
+            </h1>
+            <form class="form-horizontal" style="text-align:center; margin: 0px auto" action="views/searchresults.php" method="POST">
+                <div class="form-group">
+                    <div class="col-xs-offset-3 col-xs-5">
+                      <input type="search" name="searchvalue" class="form-control">
+                    </div>
+                    <input type="submit" class="btn btn-inverse col-xs-1" value="Find home">
                 </div>
-                <input type="submit" class="btn btn-inverse col-xs-1" value="Find home">
-            </div>
-          
-        <!--<span class="glyphicon glyphicon-search"></span>-->
-        </input>
-        </form>
+
+            <!--<span class="glyphicon glyphicon-search"></span>-->
+            </input>
+            </form>
+          </div>
+        </div>
     </body>
 </html>
 <?php
