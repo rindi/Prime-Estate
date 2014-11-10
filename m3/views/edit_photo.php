aaf89d8ec66b5716988aac8ce87266e0b132655f
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-10 03:15:19 +0000

    Store images in $all_images in edit_photo.php

--- m3/views/edit_photo.php
+++ m3/views/edit_photo.php
@@ -1,9 +1,8 @@
 <?php
 
-if(!($_SESSION['type']==2))
-{
-    die("Session 'Realtor' is NOT set.");
-}
+include("../controllers/listinhgs_controller.php");
 
+$images_controller = new listings_controller();
+$all_images = $images_controller->getImages($_GET['id']);
 
 ?>
