39d62694cd9ddd7f78ad5ac0535aad4f77ee7fa6
xli <xli@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-11 01:37:47 +0000

    add profilehandler

---
+++ m3/views/profilehandler.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    
+      <?php
+require_once ("../controllers/profile_controller.php");
+require_once ("../models/profile_model.php");
+require_once ("../controllers/users_controller.php");
+/**
+        $zip = $_POST["zip"];
+        $bed =  $_POST["bed"];
+        $bathroom  = $_POST["bathroom"];
+        $pricemax = $_POST["pricemax"];
+        $pricemin = $_POST["pricemin"];
+        $info = $_POST["info"];
+        $id = $_POST["id"];
+ */
+          
+            $dbRow['bedrooms'] = $_POST["zip"];
+            $dbRow['bathrooms'] = $_POST["bed"];
+            $dbRow['pricemax']= $_POST["pricemax"];
+            $dbRow['pricemin']= $_POST["pricemin"];
+            $dbRow['zip'] = $_POST["zip"] = $_POST["zip"];
+            $dbRow['personalinformation']  = $_POST["info"];
+            $dbRow['userid'] =  $_POST["id"];
+            $profile = new profile_model($dbRow);
+            $profilecontroler = new profile_controller();
+            $profilecontroler->updateCustomerProfile($profile);
+            header('Location: http://sfsuswe.com/~f14g03/views/profile.php');
+            
+        ?>
+   
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    
+    
+    <body>
+      
+    </body>
+</html>
