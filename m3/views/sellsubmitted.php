c1af6edf9dc0de12ebcbab3fa4b0f561f164cdfc
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-16 20:03:33 +0000

    cleaned up the sell your home listing (still needs images of houses which have been sold!)

--- m3/views/sellsubmitted.php
+++ m3/views/sellsubmitted.php
@@ -10,10 +10,10 @@ $lastname = $_POST['lastname'];
 $phone = preg_replace("/[^0-9]/", '', $_POST['phone']);
 $email = $_POST['email'];
 
-echo $firstname;
-echo $lastname;
-echo $email;
-echo $phone;
+//echo $firstname;
+//echo $lastname;
+//echo $email;
+//echo $phone;
 
 if (!filter_var($email, FILTER_VALIDATE_EMAIL))
 {
@@ -24,18 +24,16 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL))
 
 if($flag == 'true')
 {
-$input['firstname'] = $firstname;
-$input['lastname'] = $lastname;
-$input['phone'] = $phone;
-$input['email'] = $email;
-$user = new lead_model($input);
-$lead_controller = new leads_controller();
-$lead_controller->addLead($user);
-//echo "Thank you for contacting us ". $user->getFirstname() .", we will get in touch with you soon!";
+    $input['firstname'] = $firstname;
+    $input['lastname'] = $lastname;
+    $input['phone'] = $phone;
+    $input['email'] = $email;
+    $user = new lead_model($input);
+    $lead_controller = new leads_controller();
+    $lead_controller->addLead($user);
 }
-//echo "Thank you for contacting us ". $user->getFirstname() .", we will get in touch with you soon!";
+
 ?>
-<!--echo '<script type="text/javascript">alert("Thank you for contacting us, we will get in touch with you soon!");</script>';-->
 <script type="text/javascript">
     var flag = "<?php echo $flag;?>";
     document.write(flag);
