15f8daeca5508bc9f02d1854e317dbe35f845726
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-16 21:40:03 +0000

    added suggestion for search box

--- m3/index.php
+++ m3/index.php
@@ -14,7 +14,7 @@ include 'views/navbar.php';
             <form class="form-horizontal" style="text-align:center; margin: 0px auto" action="views/search_results.php" method="POST">
                 <div class="form-group">
                     <div class="col-xs-offset-3 col-xs-5">
-                      <input type="search" name="searchvalue" class="form-control">
+                      <input type="search" name="searchvalue" placeholder="Enter a City or Zipcode" class="form-control">
                     </div>
                     <label for="submit-form" class="btn btn-default btn-lg col-xs-1"><i class="glyphicon glyphicon-search"></i></label>
                     <input id="submit-form" type="submit" class="btn btn-inverse col-xs-1 hidden">
