19e37423e25475a796739e95c27549f4e43a10de
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-17 22:45:41 +0000

    transparent bg, 3 bubbles.

--- m3/index2.php
+++ m3/index2.php
@@ -1,6 +1,5 @@
 <?php
 include 'views/navbar.php';
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,14 +25,14 @@ include 'views/navbar.php';
 	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
-	
+
 	<link rel="stylesheet" href="http://sfsuswe.com/~f14g03/views/css/index.css" />
 	<link rel="stylesheet" href="./views/css/cover.css" />
 	<style>
 	    body, html { 
-	    overflow-x: hidden; 
-	    overflow-y: auto;
-	}
+		overflow-x: hidden; 
+		overflow-y: auto;
+	    }
 	</style>
     </head>
 
@@ -44,15 +43,39 @@ include 'views/navbar.php';
 		    <div class="inner cover">
 			<h1 class="cover-heading">Welcome to Prime Estate.</h1>
 			<form class="form-horizontal" style="text-align:center; margin: 0px auto" action="views/search_results.php" method="POST">
-                <div class="form-group">
-                    <div class="col-xs-offset-3 col-xs-5">
-                      <input type="search" name="searchvalue" placeholder="Enter a City or Zipcode" class="form-control">
-                    </div>
-                    <label for="submit-form" class="btn btn-default btn-lg col-xs-1"><i class="glyphicon glyphicon-search"></i></label>
-                    <input id="submit-form" type="submit" class="btn btn-inverse col-xs-1 hidden">
-                </div>
-            </form>
+			    <div class="form-group">
+				<div class="col-xs-offset-3 col-xs-5">
+				    <input type="search" name="searchvalue" placeholder="Enter a City or Zipcode" class="form-control">
+				</div>
+				
+				<label for="submit-form" class="btn btn-default btn-lg col-xs-1"><i class="glyphicon glyphicon-search"></i></label>
+				<input id="submit-form" type="submit" class="btn btn-inverse col-xs-1 hidden form-control">
+				
+			    </div>
+			</form>
 		    </div>
+		    <hr>
+		    <!-- Three columns of text below the carousel -->
+		    <div class="row" style="padding-top: 60px;">
+			<div class="col-lg-4">
+			    <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+			    <h2>Heading</h2>
+			    <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+			    <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+			</div><!-- /.col-lg-4 -->
+			<div class="col-lg-4">
+			    <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+			    <h2>Heading</h2>
+			    <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+			    <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+			</div><!-- /.col-lg-4 -->
+			<div class="col-lg-4">
+			    <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+			    <h2>Heading</h2>
+			    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+			    <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+			</div><!-- /.col-lg-4 -->
+		    </div><!-- /.row -->
 		</div>
 	    </div>
 	</div>
@@ -66,18 +89,18 @@ include 'views/navbar.php';
 	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 	<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
 	<script>
-	    (function () {
-		'use strict';
-		if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-		  var msViewportStyle = document.createElement('style')
-		  msViewportStyle.appendChild(
-		    document.createTextNode(
-		      '@-ms-viewport{width:auto!important}'
-		    )
-		  )
-		  document.querySelector('head').appendChild(msViewportStyle)
-		}
-	      })();
+            (function () {
+                'use strict';
+                if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+                    var msViewportStyle = document.createElement('style')
+                    msViewportStyle.appendChild(
+                            document.createTextNode(
+                                    '@-ms-viewport{width:auto!important}'
+                                    )
+                            )
+                    document.querySelector('head').appendChild(msViewportStyle)
+                }
+            })();
 	</script>
     </body>
 </html>
