27cc3caab91bb490c9b6939bfeb129ddb223b6ea
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-19 19:05:01 +0000

    nicefy userlist.php for admin's view

--- m3/views/userlist.php
+++ m3/views/userlist.php
@@ -5,7 +5,7 @@ include 'navbar.php';
 require_once ("../controllers/users_controller.php");
 require_once ("../models/user_model.php");
 //TYPE Real vs Cust
-$type=$_GET['type'];
+$type = $_GET['type'];
 
 
 
@@ -24,27 +24,31 @@ $type=$_GET['type'];
 
 <html>
     <style>
-        h2{
-         padding-left:500px;   
+        h1{
+	    text-align: center; 
+	    color: #12aca5;
+	    margin: 10px;		
         }
+	.table{
+	    border-color: #12aca5;
+	}
     </style>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
-    <body>
+    <body style="padding-bottom: 40px">
+	<div class="container">
+	    <?php
+	    if ($type == 1)
+		echo "<div class=\"panel panel-default\"><h1> Customers </h1></div>";
+	    else
+		echo "<div class=\"panel panel-default\"><h1> Realtors </h1></div>";
 
-        <?php
-        if ($type == 1)
-            echo "<h2> Customers </h2>";
-        else
-            echo "<h2> Realtors </h2>";
-            
 //        $value=$_POST["searchvalue"];
-     
-        #echo $query;
-        echo "<div class='results'>
-        <table class='table' style='width:90%' border='1' align='center'>
+	    #echo $query;
+	    echo "<div class='results'>
+        <table class='table' style='width:100%' align='center'>
         <thead>
         <tr>
         <th>Username</th>
@@ -55,59 +59,40 @@ $type=$_GET['type'];
         <th>Email</th>
         <th> </th>
         </tr></thead>";
-        
-        $user_controller = new users_controller();
-        
-        //if we were asked to delete a user delete them!
-        if (count($_GET)==2)
-{       {
-            $user_controller->delete($_GET['userid']);
-}       }
-        
-        
-        
-        if ($type == 1)
-            $userSet = $user_controller->getCustomers();
-        else
-            $userSet = $user_controller->getRealtors();
-        
-        foreach((array)$userSet as $userData) 
-        {
-            echo "<tbody><tr>";
+
+	    $user_controller = new users_controller();
+
+	    //if we were asked to delete a user delete them!
+	    if (count($_GET) == 2) { {
+		    $user_controller->delete($_GET['userid']);
+		}
+	    }
+
+
+
+	    if ($type == 1)
+		$userSet = $user_controller->getCustomers();
+	    else
+		$userSet = $user_controller->getRealtors();
+
+	    foreach ((array) $userSet as $userData) {
+		echo "<tbody><tr>";
 //            echo "<td><a href=\"deleteuser.php?id=\">" . $houseval . "</a>";
 //            echo "<a href=\"edit_listing.php?id=".$houseval."\"> Edit</a>";
 //            echo "</td>";
-            echo "<td>" . $userData->getUserName() . "</td>";
-            echo "<td>" . $userData->getFirstname() . "</td>";
-            echo "<td>" . $userData->getLastname() . "</td>";
-            echo "<td>" . $userData->getUserid() . "</td>";
-            echo "<td>" . $userData->getUserType() . "</td>";
-            echo "<td>" . $userData->getUserEmail() . "</td>";
-            echo "<td>" . "<a href='http://sfsuswe.com/~f14g03/views/userlist.php?type=".$type."&userid=".$userData->getUserid()."' class='btn btn-default' value='Edit Listing' type='button' onclick='return confirm("."'Are you sure you want to delete this listing?'".");'>Delete User</a>" . "</td>";
-//            echo "<td><a href='" . $mapurl . "'><img src='assets/logo/maplink.png' height='42' width='42' ></img></a></td><td>";
-//            echo "<a href = 'https://google.com'> Click Here </a></td><td>";
-//            echo "<a href = '>" . $mapurl . " target='_blank'><img src='static/map-creation.png'></img></a></td><td>";
-            #IMAGES!??!
-//            $images = $listingData->getImages();
-//            foreach((array)$images as $image) 
-//            {
-//                echo "<a href = " . $image . "><img src=" . $image . " height='42' width='42' ></img></a>";   
-//            }
-//            echo "</td></tr>";
-            
-//            $imgquery="SELECT path FROM images WHERE houseid='$houseval'";
-//            $imgresult=$con->query($imgquery);
-//            
-//            while($imgrow = mysqli_fetch_array($imgresult)) {
-//            echo "<a href = " . $imgrow['path'] . "><img src=" . $imgrow['path'] . " height='42' width='42' ></img></a>";}
-//            echo "</td></tr>";
-        }
-        
-        echo "</tbody></table></div>";
-//        if (!mysqli_query($con,$query)) {
-//            die('Error: ' . mysqli_error($con));
-//        }
-        ?>
-        
+		echo "<td>" . $userData->getUserName() . "</td>";
+		echo "<td>" . $userData->getFirstname() . "</td>";
+		echo "<td>" . $userData->getLastname() . "</td>";
+		echo "<td>" . $userData->getUserid() . "</td>";
+		echo "<td>" . $userData->getUserType() . "</td>";
+		echo "<td>" . $userData->getUserEmail() . "</td>";
+		echo "<td>" . "<a href='http://sfsuswe.com/~f14g03/views/userlist.php?type=" . $type . "&userid=" . $userData->getUserid() . "' class='btn btn-default' value='Edit Listing' type='button' onclick='return confirm(" . "'Are you sure you want to delete this listing?'" . ");'>Delete User</a>" . "</td>";
+
+	    }
+
+	    echo "</tbody></table></div>";
+
+	    ?>
+        </div>
     </body>
 </html>
