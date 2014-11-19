eb1f0747718948c4562ffd64a3b04520c40d9314
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-19 18:51:19 +0000

    a little of fixing to admin.php

--- m3/views/admin.php
+++ m3/views/admin.php
@@ -23,19 +23,19 @@ else if($usertype != 3)
     <body>
 	<div class="container-fluid">
 	    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-		<div class="panel panel-primary">
+		<div class="panel panel-default">
 		    <div class="panel-heading">
 			<h3 class="panel-title">Administrator Functions</h3>
 		    </div>
 		    <div class="panel-body">
 			<p>        
-			    <a href="http://sfsuswe.com/~f14g03/views/registration.php" class="btn btn-primary btn-lg btn-block">Add Customer</a>
+			    <a href="http://sfsuswe.com/~f14g03/views/registration.php" class="btn btn-default btn-lg btn-block">Add Customer</a>
 			</p><p>
-			    <a href="http://sfsuswe.com/~f14g03/views/userlist.php?type=1" class="btn btn-primary btn-lg btn-block">View Customers</a>
+			    <a href="http://sfsuswe.com/~f14g03/views/userlist.php?type=1" class="btn btn-default btn-lg btn-block">View Customers</a>
 			</p><p>
-			    <a href="http://sfsuswe.com/~f14g03/views/registration.php?type=2" class="btn btn-primary btn-lg btn-block">Add Realtor</a>
+			    <a href="http://sfsuswe.com/~f14g03/views/registration.php?type=2" class="btn btn-default btn-lg btn-block">Add Realtor</a>
 			</p><p>
-			    <a href="http://sfsuswe.com/~f14g03/views/userlist.php?type=2" class="btn btn-primary btn-lg btn-block">View Realtors</a>
+			    <a href="http://sfsuswe.com/~f14g03/views/userlist.php?type=2" class="btn btn-default btn-lg btn-block">View Realtors</a>
 			</p>
 		    </div>
 		</div>
