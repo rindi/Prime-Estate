2ad05aaac7834db646a1c03372071fd11fd93cdb
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-19 12:03:16 +0000

    final changes.

--- m3/views/search_results.php
+++ m3/views/search_results.php
@@ -137,8 +137,8 @@ else
 				    <br><br><br>
 				    <h4><?php echo $listingData->getCity() . ' ' . $listingData->getZip(); ?></h4>
 				    <h4>Bedrooms : <?php echo $listingData->getRooms(); ?> Bathrooms : <?php echo $listingData->getBathrooms(); ?></h4>
-				    <a class="btn btn-default" href="listing_page.php?id=<?php echo $houseval; ?>">View House <span class="glyphicon glyphicon-chevron-right"></span></a>
-				    <a class="btn btn-default" href="#">Contact Seller<span class="glyphicon glyphicon-chevron-right"></span></a>
+				    <a class="btn btn-default" href="listing_page.php?id=<?php echo $houseval; ?>"><strong>View House</strong> <span class="glyphicon glyphicon-chevron-right"></span></a>
+				    <a class="btn btn-default" href="#"><strong>Contact Seller</strong><span class="glyphicon glyphicon-chevron-right"></span></a>
 				</div>
 			    </div>
 			</div>
