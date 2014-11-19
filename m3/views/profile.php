7e05d7cf26848993d22f440f457e4e042413554f
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-19 19:50:27 +0000

    no cahnge

--- m3/views/profile.php
+++ m3/views/profile.php
@@ -18,25 +18,21 @@ require_once ("../controllers/users_controller.php");
 //$usercontroller = new users_controller();
 //$username = $_POST["login_username"];
 //$password = $_POST["login_password"];
-
 //$encryptedpassword = md5($password);
 //$userlist = $usercontroller->getUsers();
-
 //        if( $get_users = $db_connect->query("SELECT * FROM usertable") )
 //        {
 //    while( $row = $get_users->fetch(PDO::FETCH_OBJ) )
-        //foreach ($userlist as $row) 
-        //{
-          //  if( $row->getUserName() == $username && $row->getUserPassword() == $encryptedpassword )
-            //{
-              //  session_start();
-                //$_SESSION["username"] = $username;
-                //$loggedin = true;
-            //}
-        //}
+//foreach ($userlist as $row) 
+//{
+//  if( $row->getUserName() == $username && $row->getUserPassword() == $encryptedpassword )
+//{
+//  session_start();
+//$_SESSION["username"] = $username;
+//$loggedin = true;
+//}
+//}
 //        }
-        
- 
 //$profilecontroller = new profile_controller();
 //GET THE CUSTOMER ID SOMEHOW (prolly cookies)
 //$customerid = $_COOKIE['username'];
@@ -47,7 +43,6 @@ require_once ("../controllers/users_controller.php");
 //$profile->setPricemax($newValue);
 //$profile->setPricemin($newValue);
 //$profilecontroller->updateCustomerProfile($profile);
-
 //$results = $profilecontroller->getProfileResults($profile);
 //if ((array) $results)
 //{
@@ -66,32 +61,30 @@ require_once ("../controllers/users_controller.php");
     </head>
     <style>
 
-    
+
         #texttable,p{   
             margin-left: 2cm;
         }
-       #div1 {
-    margin-left: 2cm;
-    width: 500px;
-    height: 150px;
-    
-    overflow-x: hidden;
-    }
+	#div1 {
+	    margin-left: 2cm;
+	    width: 500px;
+	    height: 150px;
+
+	    overflow-x: hidden;
+	}
 
 
 
     </style>
     <body>
-        
-     <?php
-     $userid =$_SESSION["username"];
-      $profilecontroller = new profile_controller();
-      $usercontroller = new users_controller();
-     $id = $usercontroller->getUserId($userid);
-     $profile = $profilecontroller->getProfile(($id));
-    
-     
-     ?>
+
+<?php
+$userid = $_SESSION["username"];
+$profilecontroller = new profile_controller();
+$usercontroller = new users_controller();
+$id = $usercontroller->getUserId($userid);
+$profile = $profilecontroller->getProfile(($id));
+?>
         <form id = "f1" action="profilehandler.php" method="post" style="text-align:left; margin: 0px auto">
             <p><center>
                 <b><font size="5">MY PROFILE</font>
@@ -100,53 +93,52 @@ require_once ("../controllers/users_controller.php");
         <table id ="texttable">
             <tr>
                 <td>Price: Min </td>
-                <td><input id = "textfield"type="text" name="pricemin" value ="<?php echo $profile->pricemin?>"></td>
+                <td><input id = "textfield"type="text" name="pricemin" value ="<?php echo $profile->pricemin ?>"></td>
                 <td> Max &nbsp; </td>
-                <td><input id = "textfield"type="text" name="pricemax" value ="<?php echo $profile->pricemax?>" ></td>
+                <td><input id = "textfield"type="text" name="pricemax" value ="<?php echo $profile->pricemax ?>" ></td>
             </tr>
 
             <tr>
                 <td>Zip Code:</td>
-                <td><input id = "textfield"type="text" name="zip"value ="<?php echo $profile->zip?>" ></td>
+                <td><input id = "textfield"type="text" name="zip"value ="<?php echo $profile->zip ?>" ></td>
             </tr>
             <tr>
                 <td>Bedrooms:</td>
-                <td><input id = "textfield"type="text" name="bed"value ="<?php echo $profile->bedrooms?>" ></td>
+                <td><input id = "textfield"type="text" name="bed"value ="<?php echo $profile->bedrooms ?>" ></td>
             </tr>
 
             <tr>
                 <td>Bathrooms:</td>
-                <td><input id = "textfield"type="text" name="bathroom" value ="<?php echo $profile->bathrooms?>"></td>
+                <td><input id = "textfield"type="text" name="bathroom" value ="<?php echo $profile->bathrooms ?>"></td>
             </tr>
 
 
         </table>
-       
- 
+
+
         <p><b>PERSONAL INFORMATION</b> </p>
 
         <p>
-   
-        
+
         <div class="form-group" id ="div1" >
-            
-            <textarea class="form-control"  cols="60"rows="10" name = "info" id="info"><?php echo $profile->personalinformation?></textarea>
+
+            <textarea class="form-control"  cols="60"rows="10" name = "info" id="info"><?php echo $profile->personalinformation ?></textarea>
         </div>
-        </p>
-         
-         
-        
-        <input name = "id" type = "hidden"value  =<?php echo $id ?>>
-         <center><input id = "update" type="submit" value = "update" ></center>
-         
-      
-         
-        </form>
-        
-        
-        
-    
-        
+    </p>
+
+
+
+    <input name = "id" type = "hidden"value  =<?php echo $id ?>>
+    <center><input id = "update" type="submit" value = "update" ></center>
+
+
+
+</form>
+
+
+
+
+
 </body>
 
 </html>
