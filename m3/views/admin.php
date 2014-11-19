f5d0bfa5b2c187e67f2b174b0da3e23ad5ac67f7
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-19 18:42:57 +0000

    create redirects for admin.php

--- m3/views/admin.php
+++ m3/views/admin.php
@@ -2,46 +2,45 @@
 include 'navbar.php';
 
 
-if(isset($_SESSION["type"]))
-    $usertype = $_SESSION["type"];
+if (isset($_SESSION["type"]))
+$usertype = $_SESSION["type"];
 else
-    $usertype = 0;
-
-if($usertype==3)
+$usertype = 0;
+if($usertype == 2)
 {
+    header('Location: http://sfsuswe.com/~f14g03/views/dashboard.php');
+}
+else if($usertype != 3)
+{
+    header('Location: http://sfsuswe.com/~f14g03/');
+}
 ?>
+<?php if ($usertype == 3): ?>
 <html>
     <head>
-        <title> Administrator </title>
+	<title> Administrator </title>
     </head>
     <body>
-                <div class="container-fluid">
-            <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Administrator Functions</h3>
-                    </div>
-                    <div class="panel-body">
-                        <p>        
-                        <a href="http://sfsuswe.com/~f14g03/views/registration.php" class="btn btn-primary btn-lg btn-block">Add Customer</a>
-                        </p><p>
-                        <a href="http://sfsuswe.com/~f14g03/views/userlist.php?type=1" class="btn btn-primary btn-lg btn-block">View Customers</a>
-                        </p><p>
-                        <a href="http://sfsuswe.com/~f14g03/views/registration.php?type=2" class="btn btn-primary btn-lg btn-block">Add Realtor</a>
-                        </p><p>
-                        <a href="http://sfsuswe.com/~f14g03/views/userlist.php?type=2" class="btn btn-primary btn-lg btn-block">View Realtors</a>
-                        </p>
-    </div>
-                </div>
-            </div> 
-        </div>  
-                    </body>
+	<div class="container-fluid">
+	    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+		<div class="panel panel-primary">
+		    <div class="panel-heading">
+			<h3 class="panel-title">Administrator Functions</h3>
+		    </div>
+		    <div class="panel-body">
+			<p>        
+			    <a href="http://sfsuswe.com/~f14g03/views/registration.php" class="btn btn-primary btn-lg btn-block">Add Customer</a>
+			</p><p>
+			    <a href="http://sfsuswe.com/~f14g03/views/userlist.php?type=1" class="btn btn-primary btn-lg btn-block">View Customers</a>
+			</p><p>
+			    <a href="http://sfsuswe.com/~f14g03/views/registration.php?type=2" class="btn btn-primary btn-lg btn-block">Add Realtor</a>
+			</p><p>
+			    <a href="http://sfsuswe.com/~f14g03/views/userlist.php?type=2" class="btn btn-primary btn-lg btn-block">View Realtors</a>
+			</p>
+		    </div>
+		</div>
+	    </div> 
+	</div>  
+    </body>
 </html>
-<?php
-
-}
-else
-{
-    echo "Sorry, you do not have sufficient privileges to access this page.";
-}
-?>
+<?php endif;?>
