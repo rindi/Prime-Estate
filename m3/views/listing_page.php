3d3747d7b53e035a6bd05d0edcd40feb1789614b
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-19 08:23:54 +0000

    fix listing_page unclosed </div> and </html>

--- m3/views/listing_page.php
+++ m3/views/listing_page.php
@@ -76,7 +76,7 @@ $mapstring = $srcstart . $enc . $mapaddress . $end;
 			    <div class="">
 
 				<div class="panel panel-default">
-				    
+
 				    <table class="table">
 					<tr>
 					    <td>
@@ -106,145 +106,51 @@ $mapstring = $srcstart . $enc . $mapaddress . $end;
 				    </table>
 				    <div class="panel-footer">
 					<div class="clearfix">
-					<?php if (isset($_SESSION['userid'])): ?>
-					    <a href="listing_page.php?interest=<?php echo $interested; ?>&id=<?php echo $_GET['id']; ?>&userid=<?php echo $userid; ?>"
-					       class="btn btn-success pull-right" 
-					       value="Edit listing" type="button">Contact seller
-					    </a>
-					</div>
+					    <?php if (isset($_SESSION['userid'])): ?>
+    					    <a href="listing_page.php?interest=<?php echo $interested; ?>&id=<?php echo $_GET['id']; ?>&userid=<?php echo $userid; ?>"
+    					       class="btn btn-success pull-right" 
+    					       value="Edit listing" type="button">Contact seller
+    					    </a>
+    					</div>
 					<?php else: ?>
-					<div class="clearfix">
-					    <a href="newlogin.php" class="btn btn-primary pull-right">Login to contact</a>
-					</div>   
+    					<div class="clearfix">
+    					    <a href="newlogin.php" class="btn btn-primary pull-right">Login to contact</a>
+    					</div>   
 					<?php endif ?>
 				    </div>
 				</div>
-			    
-                        </div>
-                    </div>
-                </div>
 
-                <!-- 2ND WELL: MAP -->
-                <div class="well">
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <iframe
-                                width='100%'
-                                height='250'
-                                frameborder='0' style='border:0'
-<?php echo $mapstring; ?>>
-                            </iframe>
-                        </div>
-                    </div>
-                </div>
-
-
-
-		<div class="panel panel-default">
-		    <div class="panel-body">
-			<h2>Description</h2>
-			<p><?php echo $listing_model->getDescription(); ?></p>
+			    </div>
+			</div>
 		    </div>
-		    <div class="panel-footer">
-			<strong>Date posted: </strong><?php echo $listing_model->getDateAdded(); ?>
+
+		    <!-- 2ND WELL: MAP -->
+		    <div class="well">
+			<div class="row">
+			    <div class="col-xs-12">
+				<iframe
+				    width='100%'
+				    height='250'
+				    frameborder='0' style='border:0'
+				    <?php echo $mapstring; ?>>
+				</iframe>
+			    </div>
+			</div>
+		    </div>
+
+
+
+		    <div class="panel panel-default">
+			<div class="panel-body">
+			    <h2>Description</h2>
+			    <p><?php echo $listing_model->getDescription(); ?></p>
+			</div>
+			<div class="panel-footer">
+			    <strong>Date posted: </strong><?php echo $listing_model->getDateAdded(); ?>
+			</div>
 		    </div>
 		</div>
-            </div>
-        </div>
+	    </div>
+	</div>
     </div>
-
-    <!--<!DOCTYPE html>
-    <html>
-    <head>
-    <script>
-    function initialize()
-    {
-      var mapProp = {
-        center: new google.maps.LatLng(37.7118078,-122.4545622),
-        zoom:11,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-      var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-      var marker=new google.maps.Marker({
-      position:myCenter,
-      icon:'/assets/logo/PrimeEstate_Logo_Search.png'
-      });
-    
-    marker.setMap(map);
-    }
-    
-    function loadScript()
-    {
-      var script = document.createElement("script");
-      script.type = "text/javascript";
-      script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyDY0kkJiTPVd2U7aTOAwhc9ySH6oHxOIYM&sensor=false&callback=initialize";
-      document.body.appendChild(script);
-    }
-    
-    window.onload = loadScript;
-    </script>
-    </head>
-    
-    <body>
-    <div id="googleMap" style="width:500px;height:500px;"></div>
-    
-    </body>
-    </html>-->
-    <!--
-    <head>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no"/>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-    <title>Prime Estate</title>
-    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-    <script type="text/javascript">
-      var geocoder;
-      var map;
-      var address = <! -- ?php echo $mapaddress; ? >;
-      function initialize() {
-        geocoder = new google.maps.Geocoder();
-        var latlng = new google.maps.LatLng(-37.722, 122.478);
-        var myOptions = {
-          zoom: 8,
-          center: latlng,
-        mapTypeControl: true,
-        mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
-        navigationControl: true,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-        if (geocoder) {
-          geocoder.geocode( { 'address': address}, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-              if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
-              map.setCenter(results[0].geometry.location);
-    
-                var infowindow = new google.maps.InfoWindow(
-                    { content: '<b>'+address+'</b>',
-                      size: new google.maps.Size(150,50)
-                    });
-    
-                var marker = new google.maps.Marker({
-                    position: results[0].geometry.location,
-                    map: map, 
-                    title:address
-                }); 
-                google.maps.event.addListener(marker, 'click', function() {
-                    infowindow.open(map,marker);
-                });
-    
-              } else {
-                alert("No results found");
-              }
-            } else {
-              alert("Geocode was not successful for the following reason: " + status);
-            }
-          });
-        }
-      }
-    </script>
-    </head>
-    <body style="margin:0px; padding:0px;" onload="initialize()">
-     <div id="map_canvas" style="width:100%; height:100%">
-    </body>
-    </html>
-    http://stackoverflow.com/questions/15925980/using-address-instead-of-longitude-and-latitude-with-google-maps-api-->
+</html>
