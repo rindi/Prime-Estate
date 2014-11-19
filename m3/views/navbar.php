a944941bf3dc33c947cc7df8f990052d47d966d7
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-19 20:51:08 +0000

    make links in navbar <strong> for better visibility for grandmom

--- m3/views/navbar.php
+++ m3/views/navbar.php
@@ -1,9 +1,8 @@
 <?php
-if(!isset($_SESSION)) 
-{ 
-    session_start(); 
+if (!isset($_SESSION)) {
+    session_start();
 }
-if(isset($_SESSION['type']))
+if (isset($_SESSION['type']))
     $type = $_SESSION['type'];
 else
     $type = 0;
@@ -21,8 +20,8 @@ else
 	    margin-bottom: 0px;
 	}
         .footer {
-        background-color: #FFFFFF;
-        text-align: center;
+	    background-color: #FFFFFF;
+	    text-align: center;
         }
 	.navbar{
 	    background: rgba(255,255,255, 0.9);
@@ -33,84 +32,87 @@ else
     </style>
     <body>
         <nav class="navbar" role="navigation" style="margin-bottom:0px;">
-            
+
             <div class="navbar-header" >
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="navbar-collapse">
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>    
             </div>
             <a class="navbar-brand" href="http://sfsuswe.com/~f14g03/index.php">
-                    <img style="max-width:100px; margin-top: -7px;" src="http://sfsuswe.com/~f14g03/views/assets/logo/PrimeEstate_Logo_Menu.png">
-                </a>
-            <div class="navbar-collapse collapse">
-        <ul class="nav navbar-nav navbar-left"> 
-            <?php
-            if($type==0||$type==1)
-            {?>
-            <li> 
-                <a href="http://sfsuswe.com/~f14g03/views/sell.php">Sell Your Home Today!</a>
-             </li><?php
-            }
-            if($type==2)
-            {?>
-             <li>
-                <a href="http://sfsuswe.com/~f14g03/views/leads.php">Leads</a>
-            </li><?php
-            }
-            if($type==1)
-            {?>
-            <li>
-                <a href="http://sfsuswe.com/~f14g03/views/profile.php">My Profile</a>
-            </li><?php
-            }
-            if($type==2)
-            {?>
-            <li>
-                <a href="http://sfsuswe.com/~f14g03/views/dashboard.php">Realtor Dashboard</a>
-            </li><?php
-            }
-            if($type==2)
-            {?>
-            <li>
-                <a href="http://sfsuswe.com/~f14g03/views/add_listing.php">Add Listing</a>
-            </li><?php
-            }
-            if($type==3)
-            {?>
-            <li>
-                <a href="http://sfsuswe.com/~f14g03/views/admin.php">Admin</a>
-            </li><?php
-            }?>
-            </ul>
-            <ul     class="nav navbar-nav navbar-right">
-            <?php
-            if($type==1||$type==2||$type==3)
-            {?>
-            <li> 
-                <a href="http://sfsuswe.com/~f14g03/views/logout.php">Logout</a>
-            </li><?php } ?>
-            <?php 
-            if($type==0)
-            {?>
-            <li>
-                <a href="http://sfsuswe.com/~f14g03/views/registration.php">Sign Up</a>
-            </li>
-            
-            <?php }
-            if($type==0)
-            {?>
-                <li>
-                <a href="http://sfsuswe.com/~f14g03/views/newlogin.php">Login</a>
-            </li>
-            <?php }?></ul>
+		<img style="max-width:100px; margin-top: -7px;" src="http://sfsuswe.com/~f14g03/views/assets/logo/PrimeEstate_Logo_Menu.png">
+	    </a>
+            <div id="navbar-collapse" class="navbar-collapse collapse">
+		<strong>
+		<ul class="nav navbar-nav navbar-left"> 
+		    <?php
+		    if ($type == 0 || $type == 1) {
+			?>
+    		    <li> 
+    			<a href="http://sfsuswe.com/~f14g03/views/sell.php">Sell Your Home Today!</a>
+    		    </li><?php
+		}
+		if ($type == 2) {
+			?>
+    		    <li>
+    			<a href="http://sfsuswe.com/~f14g03/views/leads.php">Leads</a>
+    		    </li><?php
+		}
+		if ($type == 1) {
+			?>
+    		    <li>
+    			<a href="http://sfsuswe.com/~f14g03/views/profile.php">My Profile</a>
+    		    </li><?php
+		}
+		if ($type == 2) {
+			?>
+    		    <li>
+    			<a href="http://sfsuswe.com/~f14g03/views/dashboard.php">Realtor Dashboard</a>
+    		    </li><?php
+		}
+		if ($type == 2) {
+			?>
+    		    <li>
+    			<a href="http://sfsuswe.com/~f14g03/views/add_listing.php">Add Listing</a>
+    		    </li><?php
+		}
+		if ($type == 3) {
+			?>
+    		    <li>
+    			<a href="http://sfsuswe.com/~f14g03/views/admin.php">Admin</a>
+    		    </li><?php }
+		    ?>
+		</ul>
+		<ul     class="nav navbar-nav navbar-right">
+		    <?php
+		    if ($type == 1 || $type == 2 || $type == 3) {
+			?>
+    		    <li> 
+    			<a href="http://sfsuswe.com/~f14g03/views/logout.php">Logout</a>
+    		    </li><?php } ?>
+		    <?php
+		    if ($type == 0) {
+			?>
+    		    <li>
+    			<a href="http://sfsuswe.com/~f14g03/views/registration.php">Sign Up</a>
+    		    </li>
+
+		    <?php
+		    }
+		    if ($type == 0) {
+			?>
+    		    <li>
+    			<a href="http://sfsuswe.com/~f14g03/views/newlogin.php">Login</a>
+    		    </li>
+		    <?php } ?></ul>
+		</strong>
             </div>
         </nav>
         <footer>
-        <div class="footer navbar-fixed-bottom">
-        <a href="http://sfsuswe.com/~f14g03/views/policy.php">San Francisco State University Software Engineering Project, Fall 2014.  For Demonstration Purposes Only</a>
-        </div>
+	    <div class="footer navbar-fixed-bottom">
+		<a href="http://sfsuswe.com/~f14g03/views/policy.php">San Francisco State University Software Engineering Project, Fall 2014.  For Demonstration Purposes Only</a>
+	    </div>
         </footer>
     </body>
 </html>
