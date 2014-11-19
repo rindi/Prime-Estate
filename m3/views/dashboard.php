6562219a032b37e18d9df64ed31414197443692c
alyash <alyash@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-19 19:35:03 +0000

    nicefy dashboard

--- m3/views/dashboard.php
+++ m3/views/dashboard.php
@@ -1,20 +1,32 @@
 <?php
 include 'navbar.php';
-if( $_SESSION['type'] != 2 )
-{
+if ($_SESSION['type'] != 2) {
     header('Location: http://sfsuswe.com/~f14g03');
 }
 ?>
 <html>
     <head>
         <title> Realtor </title>
+	<style>
+	    h1{
+		color: #12ACA5;
+		margin: 20px;
+		text-align: center;
+		font-weight: bold;
+	    }
+	</style>
     </head>
     <body>
-        <h1 align="center">
-            Realtor
-        </h1>
-        <a href="http://sfsuswe.com/~f14g03/views/registration.php" class="btn btn-default">Add Customer</a>
-        <a href="http://sfsuswe.com/~f14g03/views/interestedcustomers.php" class="btn btn-default">View Interested Customers</a>
-        <a href="http://sfsuswe.com/~f14g03/views/add_listing.php" class="btn btn-default">Add listings</a>
+	<div class="container">
+	    <div class="panel panel-default">
+		<h1>
+		    Realtor
+		</h1>
+	    </div>
+
+	    <a href="http://sfsuswe.com/~f14g03/views/registration.php" class="btn btn-default">Add Customer</a>
+	    <a href="http://sfsuswe.com/~f14g03/views/interestedcustomers.php" class="btn btn-default">View Interested Customers</a>
+	    <a href="http://sfsuswe.com/~f14g03/views/add_listing.php" class="btn btn-default">Add listings</a>
+	</div>
     </body>
 </html>
