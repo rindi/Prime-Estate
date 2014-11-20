d1189cb34c6d1ce619fc745452e2019e000e912d
xli <xli@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-20 03:00:05 +0000

    new verison profile

--- m3/views/profile.php
+++ m3/views/profile.php
@@ -5,141 +5,145 @@ require_once ("../controllers/profile_controller.php");
 require_once ("../models/profile_model.php");
 require_once ("../controllers/users_controller.php");
 
-//SET THESE VARIABLES
-//$dbRow['bedrooms'];
-//$dbRow['bathrooms'];
-//$dbRow['pricemax'];
-//$dbRow['pricemin'];
-//$dbRow['zip'];
-//$dbRow['personalinformation'];
-//$dbRow['userid'];
-//CALL THIS
-//$profile = new profile_model($dbRow);
-//$usercontroller = new users_controller();
-//$username = $_POST["login_username"];
-//$password = $_POST["login_password"];
-//$encryptedpassword = md5($password);
-//$userlist = $usercontroller->getUsers();
-//        if( $get_users = $db_connect->query("SELECT * FROM usertable") )
-//        {
-//    while( $row = $get_users->fetch(PDO::FETCH_OBJ) )
-//foreach ($userlist as $row) 
-//{
-//  if( $row->getUserName() == $username && $row->getUserPassword() == $encryptedpassword )
-//{
-//  session_start();
-//$_SESSION["username"] = $username;
-//$loggedin = true;
-//}
-//}
-//        }
-//$profilecontroller = new profile_controller();
-//GET THE CUSTOMER ID SOMEHOW (prolly cookies)
-//$customerid = $_COOKIE['username'];
-//GET THEIR PROFILE
-//$profile = $profilecontroller->getProfile($_SESSION["username"]);
-//UPDATE THEIR PROFILE WITH WHATEVER CHANGES THEY MAKE
-//$newValue = 2000;
-//$profile->setPricemax($newValue);
-//$profile->setPricemin($newValue);
-//$profilecontroller->updateCustomerProfile($profile);
-//$results = $profilecontroller->getProfileResults($profile);
-//if ((array) $results)
-//{
-//foreach ((array) $results as $row) 
-//{
-////    var_dump($row); 
-//    echo $row->getAddress();
-//}
-////}
-?>
-
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <style>
-
-
-        #texttable,p{   
-            margin-left: 2cm;
-        }
-	#div1 {
-	    margin-left: 2cm;
-	    width: 500px;
-	    height: 150px;
-
-	    overflow-x: hidden;
-	}
-
-
-
-    </style>
-    <body>
-
-<?php
 $userid = $_SESSION["username"];
 $profilecontroller = new profile_controller();
 $usercontroller = new users_controller();
 $id = $usercontroller->getUserId($userid);
+
 $profile = $profilecontroller->getProfile(($id));
 ?>
-        <form id = "f1" action="profilehandler.php" method="post" style="text-align:left; margin: 0px auto">
-            <p><center>
-                <b><font size="5">MY PROFILE</font>
-            </center></p>
-        <p>SEARCH INFORMATION </p>
-        <table id ="texttable">
-            <tr>
-                <td>Price: Min </td>
-                <td><input id = "textfield"type="text" name="pricemin" value ="<?php echo $profile->pricemin ?>"></td>
-                <td> Max &nbsp; </td>
-                <td><input id = "textfield"type="text" name="pricemax" value ="<?php echo $profile->pricemax ?>" ></td>
-            </tr>
+<html>
+    <head>
 
-            <tr>
-                <td>Zip Code:</td>
-                <td><input id = "textfield"type="text" name="zip"value ="<?php echo $profile->zip ?>" ></td>
-            </tr>
-            <tr>
-                <td>Bedrooms:</td>
-                <td><input id = "textfield"type="text" name="bed"value ="<?php echo $profile->bedrooms ?>" ></td>
-            </tr>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-            <tr>
-                <td>Bathrooms:</td>
-                <td><input id = "textfield"type="text" name="bathroom" value ="<?php echo $profile->bathrooms ?>"></td>
-            </tr>
+        <title>Prime Estate - My Profile Results</title>
 
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
 
-        </table>
+    </head>
 
-
-        <p><b>PERSONAL INFORMATION</b> </p>
-
-        <p>
-
-        <div class="form-group" id ="div1" >
-
-            <textarea class="form-control"  cols="60"rows="10" name = "info" id="info"><?php echo $profile->personalinformation ?></textarea>
-        </div>
-    </p>
+    <!--<div class="row">-->
+    <!--<div class="col-md-4 col-md-offset-4">-->
+    <div class="container-fluid">
+        <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><?php echo $userid;?>'s Profile</h3>
+                </div>
+                <div class<="panel-body">
+                    <form class="form-horizontal" role="form" action="profilehandler.php" method="post" >
+                        <!--<fieldset>-->
+                        <!-- Form Name -->
+                        <!--<legend>Home Preferences</legend>-->
 
 
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" for="textinput">Price</label>
+                            <div class="col-sm-4">
+                                <input type="text" name ="pricemin"
+                                <?php
+                                if ($profile->pricemin == 0)
+                                    echo 'placeholder="Lower Range"';
+                                else
+                                    echo 'value =' . $profile->pricemin;
+                                ?>>
+                            </div>
 
-    <input name = "id" type = "hidden"value  =<?php echo $id ?>>
-    <center><input id = "update" type="submit" value = "update" ></center>
+                            <label class="col-sm-2 control-label" for="textinput">Maximum</label>
+                            <div class="col-sm-4">
+                                <input type="text" name ="pricemax"
+                                <?php
+                                if ($profile->pricemax == 0)
+                                    echo 'placeholder="Upper Range"';
+                                else
+                                    echo 'value =' . $profile->pricemax;
+                                ?>>
+
+                            </div>
+                        </div>
+
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" for="textinput">City</label>
+                            <div class="col-sm-10">
+                                <input type="text" name ="city" placeholder="City"<?php echo 'value='. $profile->city ?> >
+                            </div>
+                        </div>
 
 
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" for="textinput">Zip</label>
+                            <div class="col-sm-10">
+                                <input type="text" name ="zip"                
+                                <?php
+                                if ($profile->zip == 0)
+                                    echo 'placeholder="Zip / Postal Code"';
+                                else
+                                    echo 'value =' . $profile->zip;
+                                ?>>
+                            </div>
+                        </div>
 
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" for="textinput">Bedrooms</label>
+                            <div class="col-sm-10">
+                                <input type="text" name ="bed"
+                                <?php
+                                if ($profile->bedrooms == 0)
+                                    echo 'placeholder="Bedrooms"';
+                                else
+                                    echo 'value =' . $profile->bedrooms;
+                                ?>>
+                            </div>
+                        </div>
+
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" for="textinput">Bathrooms</label>
+                            <div class="col-sm-10">
+                                <input type="text" name ="bathroom"
+                                <?php
+                                if ($profile->bathrooms == 0)
+                                    echo 'placeholder="Bathrooms"';
+                                else
+                                    echo 'value =' . $profile->bathrooms;
+                                ?>>            
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" for="textinput">Details</label>
+                            <div class="col-sm-10">
+                                <input type="textarea"  name = "info" placeholder="Please enter your personal preferences here" 
+                                <?php echo 'value=' . $profile->personalinformation ?>>
+                            </div>
+                            <input name = "id" type = "hidden"value  =<?php echo $id ?>>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <div class="pull-right">
+                                    <button type="submit" class="btn btn-default">Cancel</button>
+                                    <button type="submit" class="btn btn-primary">Save</button>
+                                    <button type="submit" formaction="profile_search.php"class="btn btn-success">Search</button>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div> 
+    </div>  
+    <!--</fieldset>-->
 </form>
-
-
-
-
-
-</body>
-
+<!--    </div> /.col-lg-12 
+</div> /.row -->
 </html>
-
