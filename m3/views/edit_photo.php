60ffcb3c81a80e41b8fda48725e8eb56f08bc939
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-21 05:29:52 +0000

    can now upload more photos to a listing in edit listing

--- m3/views/edit_photo.php
+++ m3/views/edit_photo.php
@@ -4,8 +4,7 @@ require '../controllers/listings_controller.php';
 $images_controller = new listings_controller();
 $all_images = $images_controller->getImages($_GET['id']);
 
-if( $_GET['removepath'] )
-{
+if ($_GET['removepath']) {
     $images_controller->removeImage($_GET['removepath']);
     $all_images = $images_controller->getImages($_GET['id']);
 }
@@ -14,19 +13,19 @@ if( $_GET['removepath'] )
 
 <div class="container">
     <div class="row">
+        <a href="http://sfsuswe.com/~f14g03/views/edit_listing.php?id=<?php echo $_GET['id']; ?>">
+            <button class="btn btn-default" value="&laquo; Back">Back to Listing</button>
+        </a>
+        <a href="http://sfsuswe.com/~f14g03/views/upload.php?id=<?php echo $_GET['id']; ?>">
+            <button class="btn btn-default" value="&laquo; Back">Add a Photo</button>
+        </a>
         <?php for ($i = 0; $i < count($all_images); $i++): ?>
             <div class="col-xs-6 col-md-3">
                 <a class="thumbnail">
                     <img src="<?php echo $all_images[$i]; ?>" alt="...">
                 </a>
-                
-                <a href="http://sfsuswe.com/~f14g03/views/edit_listing.php?id=<?php echo $_GET['id'];?>">
-                    <button class="btn btn-default" value="&laquo; Back">Back to Listing</button>
-                </a>
-                <a href="http://sfsuswe.com/~f14g03/views/upload.php?id=<?php echo $_GET['id'];?>">
-                    <button class="btn btn-default" value="&laquo; Back">Add a Photo</button>
-                </a>
-                <a href="edit_photo.php?id=<?php echo $_GET['id'];?>&removepath=<?php echo $all_images[$i];?>">
+
+                <a href="edit_photo.php?id=<?php echo $_GET['id']; ?>&removepath=<?php echo $all_images[$i]; ?>">
                     <button class="btn btn-danger" value="Remove">Remove</button>
                 </a>
             </div>
