a458a0a7217ed0c0b210f800608a8f585769f5f3
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-21 15:10:08 +0000

    commented all controllers and cleared uneccesary comments

--- m3/controllers/interest_controller.php
+++ m3/controllers/interest_controller.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once ("../controllers/controller.php");
 require_once ("../controllers/users_controller.php");
 require_once ("../models/user_model.php");
@@ -6,30 +7,28 @@ require_once ("../models/user_model.php");
 /**
  * User Controller Class
  */
-class interest_controller extends controller
-{
+class interest_controller extends controller {
+
     /**
      * Constructor
      */
-    public function __construct( ) 
-    {
+    public function __construct() {
         parent::__construct();
     }
-    
+
     /**
-     * get all users from the table
+     * Get interested customers function
+     * @param type $listing
      * @return \user_model
      */
-    public function getInterestedCustomers($listing)
-    {   
+    public function getInterestedCustomers($listing) {
         $sql = "SELECT * FROM interestedcustomers WHERE id = '$listing'";
         $res = $this->db_connect->query($sql);
-        foreach($res as $row)
-        {
-            
+        foreach ($res as $row) {
+
             $user_controller = new users_controller();
             $temp = $user_controller->getUserInfo($row['userid']);
-            
+
             $tempuser = new user_model($temp);
             $tempuser->setContactDate($row['date']);
             $dataSet[] = $tempuser;
@@ -39,20 +38,22 @@ class interest_controller extends controller
         else
             return null;
     }
+
     /**
-     * get all users from the table
-     * @return \user_model
+     * Express interest in a property
+     * @param type $uid
+     * @param type $listing
      */
-    public function expressInterest($uid,$listing)
-    {   
+    public function expressInterest($uid, $listing) {
         $sql = "INSERT INTO interestedcustomers(userid, id, date) VALUES (
             :userid, :id, :date)";
-                                       
+
         $stmt = $this->db_connect->prepare($sql);
-        $stmt->bindParam(':userid', $uid, PDO::PARAM_INT);       
-        $stmt->bindParam(':id', $listing, PDO::PARAM_INT); 
-        $stmt->bindParam(':date', date("Y/m/d"), PDO::PARAM_STR);  
-        
-        $stmt->execute();  
+        $stmt->bindParam(':userid', $uid, PDO::PARAM_INT);
+        $stmt->bindParam(':id', $listing, PDO::PARAM_INT);
+        $stmt->bindParam(':date', date("Y/m/d"), PDO::PARAM_STR);
+
+        $stmt->execute();
     }
+
 }
--- m3/controllers/users_controller.php
+++ m3/controllers/users_controller.php
@@ -1,40 +1,36 @@
 <?php
+
 require_once ("../controllers/controller.php");
 require_once ("../controllers/interest_controller.php");
 require_once ("../controllers/profile_controller.php");
 require_once ("../models/user_model.php");
 
-if( isset($_COOKIE['username']) )
-{
+if (isset($_COOKIE['username'])) {
     $loggedin = true;
     $loggedinas = $_COOKIE['username'];
-}
-else
-{
+} else {
     $loggedin = false;
 }
+
 /**
  * User Controller Class
  */
-class users_controller extends controller
-{
+class users_controller extends controller {
+
     /**
      * Constructor
      */
-    public function __construct( ) 
-    {
+    public function __construct() {
         parent::__construct();
     }
-    
+
     /**
      * get all users from the table
      * @return \user_model
      */
-    public function getUsers()
-    {   
+    public function getUsers() {
         $sql = "SELECT * from usertable";
-        foreach( parent::$this->db_connect->query($sql) as $row )
-        {
+        foreach (parent::$this->db_connect->query($sql) as $row) {
             $dataSet[] = new user_model($row);
         }
 
@@ -43,85 +39,79 @@ class users_controller extends controller
         else
             return null;
     }
-    
+
     /**
      * get a user's info from the table
      * @return \user_model
      */
-    public function getUserInfo($userid)
-    {   
+    public function getUserInfo($userid) {
         $sql = "SELECT * FROM usertable WHERE userid = '$userid'";
-        foreach(parent::$this->db_connect->query($sql) as $row )
-        {
+        foreach (parent::$this->db_connect->query($sql) as $row) {
             return $row;
         }
     }
-    public function getUserId($username)
-    {
-          $sql = "SELECT * FROM usertable WHERE username = '$username'";
-          $data;
-        foreach(parent::$this->db_connect->query($sql) as $row )
-        {
-           $data = $row;
+
+    /**
+     * Get a users id from their username
+     * @param type $username
+     * @return type
+     */
+    public function getUserId($username) {
+        $sql = "SELECT * FROM usertable WHERE username = '$username'";
+        $data;
+        foreach (parent::$this->db_connect->query($sql) as $row) {
+            $data = $row;
         }
         return $data["userid"];
-        
-        
     }
 
-
-    public function getUserNameInfo($username)
-    {   
+    /**
+     * Get a users id from their username
+     * @param type $username
+     * @return type
+     */
+    public function getUserNameInfo($username) {
         $sql = "SELECT * FROM usertable WHERE username = '$username'";
-        foreach(parent::$this->db_connect->query($sql) as $row )
-        {
+        foreach (parent::$this->db_connect->query($sql) as $row) {
             return $row;
         }
     }
-    
+
     /**
      * Add a user to the table
      * @param type $input
      */
-    public function addUser($input)
-    {
+    public function addUser($input) {
         $sql = "INSERT INTO 
                 usertable (username, password, type, email, firstname, lastname) 
                 VALUES (:username, :password, :type, :email, :firstname, :lastname)";
-        
+
         $stmt = $this->db_connect->prepare($sql);
-        $stmt->bindParam(':username', $input->getUserName(), PDO::PARAM_STR);       
-        $stmt->bindParam(':password', $input->getUserPassword(), PDO::PARAM_STR); 
-        $stmt->bindParam(':type', $input->getUserType(), PDO::PARAM_STR); 
-        $stmt->bindParam(':email', $input->getUserEmail(), PDO::PARAM_STR);   
-        $stmt->bindParam(':firstname', $input->getFirstname(), PDO::PARAM_STR); 
-        $stmt->bindParam(':lastname', $input->getLastname(), PDO::PARAM_STR);   
-        
+        $stmt->bindParam(':username', $input->getUserName(), PDO::PARAM_STR);
+        $stmt->bindParam(':password', $input->getUserPassword(), PDO::PARAM_STR);
+        $stmt->bindParam(':type', $input->getUserType(), PDO::PARAM_STR);
+        $stmt->bindParam(':email', $input->getUserEmail(), PDO::PARAM_STR);
+        $stmt->bindParam(':firstname', $input->getFirstname(), PDO::PARAM_STR);
+        $stmt->bindParam(':lastname', $input->getLastname(), PDO::PARAM_STR);
+
         $stmt->execute();
         echo 'User Registered.';
 
         $username = $input->getUserName();
         $sql = "SELECT * FROM usertable WHERE username = '$username'";
-//        $rows = parent::$this->db_connect->query($sql);
-//        var_dump($rows[0]);
-        foreach(parent::$this->db_connect->query($sql) as $row )
-        {
-//            var_dump($row);
+        foreach (parent::$this->db_connect->query($sql) as $row) {
             $custController = new profile_controller();
             $custController->newProfile($row['userid']);
         }
     }
-    
-    
+
     /**
-     * get all users from the table
+     * get all realtors from the table
      * @return \user_model
      */
-    public function getRealtors()
-    {   
+    public function getRealtors() {
         $sql = "SELECT * from usertable WHERE type = '2'";
-        foreach( parent::$this->db_connect->query($sql) as $row )
-        {
+        foreach (parent::$this->db_connect->query($sql) as $row) {
             $dataSet[] = new user_model($row);
         }
 
@@ -130,16 +120,14 @@ class users_controller extends controller
         else
             return null;
     }
-    
+
     /**
-     * get all users from the table
+     * get all customers from the table
      * @return \user_model
      */
-    public function getCustomers()
-    {   
+    public function getCustomers() {
         $sql = "SELECT * from usertable WHERE type = '1'";
-        foreach( parent::$this->db_connect->query($sql) as $row )
-        {
+        foreach (parent::$this->db_connect->query($sql) as $row) {
             $dataSet[] = new user_model($row);
         }
 
@@ -148,17 +136,16 @@ class users_controller extends controller
         else
             return null;
     }
-    
-    
+
     /**
-     * get all users from the table
+     * get a user from the table
      * @return \user_model
      */
-    public function delete($userid)
-    {   
+    public function delete($userid) {
         $sql = "DELETE FROM usertable WHERE userid = :userid";
-        $stmt = $this->db_connect->prepare($sql); 
-        $stmt->bindParam(':userid', $userid, PDO::PARAM_INT);   
+        $stmt = $this->db_connect->prepare($sql);
+        $stmt->bindParam(':userid', $userid, PDO::PARAM_INT);
         $stmt->execute();
     }
+
 }
