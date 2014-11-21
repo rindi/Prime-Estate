09f7f49b6fe1e88beec615c02a7d1b41360c1a93
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-21 15:17:41 +0000

    commented all models and cleared uneccesary comments

--- m3/models/user_model.php
+++ m3/models/user_model.php
@@ -3,16 +3,18 @@
 /**
  * user_model Class
  */
-class user_model
-{
+class user_model {
+
     //fields for userdata class
     public $username, $password, $userid, $type, $email, $date, $firstname, $lastname;
-    
-    public function __construct($dbRow)
-    { 
 
-        if (count($dbRow)>6)
-        {
+    /**
+     * 
+     * @param type $dbRowuser_model constructor
+     */
+    public function __construct($dbRow) {
+
+        if (count($dbRow) > 6) {
             $this->userid = $dbRow['userid'];
         }
         $this->username = $dbRow['username'];
@@ -21,76 +23,64 @@ class user_model
         $this->type = $dbRow['type'];
         $this->firstname = $dbRow['firstname'];
         $this->lastname = $dbRow['lastname'];
-
     }
-    public function getUserName()
-    {
+
+    public function getUserName() {
         return $this->username;
     }
-    public function setUserName($username)
-    {
+
+    public function setUserName($username) {
         $this->username = $username;
     }
-    public function getUserPassword()
-    {
+
+    public function getUserPassword() {
         return $this->password;
     }
-    public function getUserId()
-    {
+
+    public function getUserId() {
         return $this->userid;
     }
-    public function getUserType()
-    {
+
+    public function getUserType() {
         return $this->type;
     }
-    public function getUserEmail()
-    {
+
+    public function getUserEmail() {
         return $this->email;
     }
-    public function setUserEmail($email)
-    {
+
+    public function setUserEmail($email) {
         $this->email = $email;
     }
-    public function setUserPassword($password)
-    {
+
+    public function setUserPassword($password) {
         $this->password = $password;
     }
-    
-    public function setContactDate($date)
-    {
+
+    public function setContactDate($date) {
         $this->date = $date;
     }
-    public function getContactDate()
-    {
+
+    public function getContactDate() {
         return $this->date;
     }
-//    public function testAddUser()
-//    {
-//        $this->username = "test_un";
-//        $this->password = "test_pw";
-//        $this->type = "test_ut";
-//        $this->email = "test_em";
-//    }
-    
-    public function getFirstname()
-    {
+
+    public function getFirstname() {
         return $this->firstname;
     }
-    public function getLastname()
-    {
+
+    public function getLastname() {
         return $this->lastname;
     }
-    public function setFirstname($firstname)
-    {
+
+    public function setFirstname($firstname) {
         $this->firstname = $firstname;
     }
-    public function setLastname($lastname)
-    {
+
+    public function setLastname($lastname) {
         $this->lastname = $lastname;
     }
-    
-    
-    
+
 }
 
 ?>
