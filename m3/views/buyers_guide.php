c759491bb168be621bc3f47b2693c040d74581d0
rindi <rindi@users.noreply.github.com>
2014-11-21 08:23:26 +0000

    Formatting changes to buyers guide.

--- m3/views/buyers_guide.php
+++ m3/views/buyers_guide.php
@@ -9,109 +9,77 @@ include 'navbar.php';
 </head>
 <body>
 
- <style type="text/css"> 
-    body {font: 2em verdana; background: #FFFFFF; }
-  #div_1{
-       
-    
-       padding: 20px 5px 20px 5px; 
-      
-    }
-   
-    
- 
-   p { text-align: justify; }
+<div style="margin: auto; width: 90%; max-width: 1000px; background: #FCFCFC; padding: 15px">
 
-    
-    ul {  margin: 0; padding: 0; }
-    li { list-style-type: none; }
-   .club { font-size: 1.75em; font-weight: bold; color: #007000 }
-  
-  </style>
-</head>
-<body>
+<center><h2>Factors to consider when buying a house</h2></center>
 
-<div style="margin: auto; width: 90%; max-width: 1000px; padding: 30px" >
-
-    <center><h2><u>knowledge of House Purchasing</u></h2></center>
-
-<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Most people are looking forward to to have their own dream houses in their live. Also, they usually spend most of their savings in their live to buy their favourite houses. Therefore, it is very important to get everything ready before buying houses.   Especially, When buyers can clearly understand  some the knowledge of house purchasing  which can let buyers likely protect their own  benefits and reduce their  unnecessary money and time lost.</p>
+<p>Most people are looking forward to to have their own dream houses in their live. Also, they usually spend most of their savings in their live to buy their favourite houses. Therefore, it is very important to get everything ready before buying houses.   Especially, When buyers can clearly understand  some the knowledge of house purchasing  which can let buyers likely protect their own  benefits and reduce their  unnecessary money and time lost.</p>
 
 <h4><span class="club">&#8962;</span> The guides of looking at your houses  </h4>
 
-<p>(A) By the general environments: regional environment, transportation network, the type of &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;housing, public works,schools, market and other factors.</p>
-<p>(B) By the narrow environments: neighbourhood, public facilities, parking lots, housing &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;conditions and other factors.</p>
+<p>(A) By the general environments: regional environment, transportation network, the type of housing, public works, schools, market and other factors.</p>
+<p>(B) By the narrow environments: neighbourhood, public facilities, parking lots, housing conditions and other factors.</p>
 
 <h4><span class="club">&#8962;</span> (A) General environment</h4>
+<ol>
+    <li>Regional environment</li>
+    <ul>
+<li>Neighborhood quality of life is good? </li>
+<li>Standards of residential construction?</li>
+<li>Porn industry around?</li>
+<li>Serious river pollution?</li>
+<li>Noise pollution like railroad?</li> 
+<li>Any high-voltage tower nearby? </li>
+<li>Grave area?</li>
+<li>Gas stations?</li>
+</ul>
+<li>Transportation network</li>
+<ul> <li>Integrity of regional transportation system is directly related to the home buyers' work, school, and whether they easily and quickly reach their destinations. Therefore, in consideration of the traffic problem, homebuyers should understand the convenience of the conjunctions roads, so that it is easy to control the good traffic conditions every day and prevent wasting too much time.
+    </li></ul>
+<li>Types of houses</li>
+<ul> 
+<li>Single Hose: No HOA fee. </li>
+<li>Townhouse:  HOA fee.</li>
+<li>Condominium: Townhouse: HOA fee.</li>
+<li>***HOMEOWNERS ASSOCIATION FEE (HOA FEE) : An amount of money that must be paid monthly   
+    by owners of certain types of residential property to an organization that assists with 
+    maintaining and improving that property and others.</li>
+	</ul> 
 
-<p><b>Regional environment</b></p>
-<p>&nbsp;&nbsp;&nbsp;&#10045;&nbsp;Neighborhood quality of life is good? </p>
-<p>&nbsp;&nbsp;&nbsp;&#10045;&nbsp;Standards of residential construction?</p>
-<p>&nbsp;&nbsp;&nbsp;&#10045;&nbsp;Porn industry around?</p>
-<p>&nbsp;&nbsp;&nbsp;&#10045;&nbsp;Serious river pollution?</p>
-<p>&nbsp;&nbsp;&nbsp;&#10045;&nbsp;Noise pollution like railroad?</p> 
-<p>&nbsp;&nbsp;&nbsp;&#10045;&nbsp;Any high-voltage tower nearby? </p>
-<p>&nbsp;&nbsp;&nbsp;&#10045;&nbsp;Grave area?</p>
-<p>&nbsp;&nbsp;&nbsp;&#10045;&nbsp;Gas stations? ...</p>
+<li>Public Works</li>
+<li>Home buyers should consider value of the house in future, and then they should pay attention to the living conditions of future or existing adequacy of public construction with the government, such as ? hospitals, parks, stadiums, community centers, cultural centers, art galleries, super market, playground, child-care centers and so on.</li>
 
-<p><b>Transportation network</b></p>
-<p>&nbsp;&nbsp;Integrity of regional transportation system is directly related to the homebuyers' work, &nbsp;school, and whether they easily and quickly reach their destinations. Therefore, in &nbsp;&nbsp;consideration of the traffic problem, homebuyers should understand the convenience of &nbsp;&nbsp;the conjunctions roads, so that it is easy to control the good traffic conditions every day &nbsp;&nbsp;and prevent to waste too much time.</p>
-
-<p><b>Types of houses</b></p>
-
-<p>&nbsp;&nbsp;&nbsp;&#10045;&nbsp;Single Hose: No HOA fee. </p>
-<p>&nbsp;&nbsp;&nbsp;&#10045;&nbsp;Townhouse:  HOA fee.</p>
-<p>&nbsp;&nbsp;&nbsp;&#10045;&nbsp;Condominium: Townhouse: HOA fee.</p>
-
-<p>***HOMEOWNERS ASSOCIATION FEE (HOA FEE) : An amount of money that must be paid monthly   
-      by owners of certain types of residential property to an organization that assists with 
-      maintaining and improving that property and others.</p>
-	
-<p><b>Public Works</b></p>
-<p>&nbsp;&nbsp;Homebuyers should consider value of the house in future, and then they should pay &nbsp;&nbsp;&nbsp;attention to the living conditions of future or existing adequacy of public construction with &nbsp;&nbsp;&nbsp;the government, such as  hospitals, parks, stadiums, community centers, cultural centers, &nbsp;&nbsp;&nbsp;art galleries, super market, playground, child-care centers and so on.</p>
-
-<p><b>Schools and markets </b></p>
-<p>&nbsp;&nbsp;&#10045;&nbsp;Good reputation Schools not only provide a good educational environment but also &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;increase the value of properties in the future.</p>
-<p>&nbsp;&nbsp;&#10045;&nbsp;If homes can nearby markets, supermarkets, shopping centers, any busy professional &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;people, can also reduce the number of unnecessary time.</p>
-
+<li>Schools and markets </li>
+<li>Good reputation </li><ul><li>Schools not only provide a good educational environment but also increase the value of properties in the future.</li>
+<li>If homes can nearby markets, supermarkets, shopping centers, any busy professional women, can also reduce the number of unnecessary time.</p>
+    </ul>
 <h4><span class="club">&#8962;</span> (B)Narrow environments</h4>
 
-<p><b>Regional environment</b></p>
+<p>1.Regional environment</p>
 
-<p>&nbsp;&#10045;&nbsp;For some purposes around the house environment, homebuyers should go in the &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;daytime and at night to make an observation, include - noise, garbage problem, vendors &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;like pub and so on.</p>
+<p>For some purposes around the house environment, home buyers should go in the daytime and at night to make an observation, include - noise, garbage problem, vendors like pub and so on.</p>
 
-<p><b>Parking</b></p>
-<p>&nbsp;&nbsp;&nbsp;&#10045;&nbsp;Parking is a big issue which has been troubled by modern families.</p>
-<p>&nbsp;&nbsp;&nbsp;&#10045;&nbsp;Homebuyers should check how easy the street parking is.</p>
-<p>&nbsp;&nbsp;&nbsp;&#10045;&nbsp;Parking is exclusive or shared.</p>
-<p>&nbsp;&nbsp;&nbsp;&#10045;&nbsp;Homebuyers need to figure how much a parking spot costs if they need.</p>
+<p>2.Parking</p>
+<p>Parking is a big issue which has been troubled by modern families.</p>
+<p>Home buyers should check how easy the street parking is.</p>
+<p>Parking is exclusive or shared.</p>
+<p>Home buyers need to figure how much a parking spot costs if they need.</p>
 
 <h4><span class="club">&#8962;</span> House Mortgage/Loan</h4>
-<p>Basically, a default value of rate is 30 years of 6.5%, but actually homebuyers' home loan interest rate base on your credit history and other various conditions. Each person's rates are not the same. Generally if credit score is not high enough, the loan interest rate is relatively higher. Before you loan, the bank will first consult your consent and check your credit score.</p>
+<p>Basically, a default value of rate is 30 years of 6.5%, but actually home buyers' home loan interest rate base on your credit history and other various conditions. Each person's rates are not the same. Generally if credit score is not high enough, the loan interest rate is relatively higher. Before you loan, the bank will first consult your consent and check your credit score.</p>
 
-<p>&#10045;&nbsp;Credit score list</p>
-<p>&nbsp;&nbsp;&nbsp;750 ~ 850:Excellent.</p>
-<p>&nbsp;&nbsp;&nbsp;660 ~ 749:Good. </p>
-<p>&nbsp;&nbsp;&nbsp;620 ~ 659:Fair. </p>
-<p>&nbsp;&nbsp;&nbsp;350 ~ 619:Poor.</p>
+<p>&#10045;Credit score list</p>
+<p>750 ~ 850:Excellent.</p>
+<p>660 ~ 749:Good. </p>
+<p>620 ~ 659:Fair. </p>
+<p>350 ~ 619:Poor.</p>
 
-<p>On the other hand, if you are the first time homebuyers, you can get a lower interest rate with first-time homebuyer education program.</p>
-
-
-<center><span class="club">&#8962;</span><span class="club">&#8962;</span><span class="club">&#8962;</span></center>
-
+<p>On the other hand, if you are a first time home buyer, you can get a lower interest rate with first-time home buyer education program.</p>
 
 <div style="text-align: center; background: #826B34"><a href=""><img src="" border="0" alt=""></a></div>
-<div id = "div_1">
-          
-            <input id="backb" class="btn btn-default"  type="button" value="Back" onClick="history.go(-1);return true;">
+            <input class="btn btn-default" style="float: left;" type="button" value="Back" onClick="history.go(-1);return true;">
 </div>
-</div>
-
-
-
-
- <br><br><br>
+    <br><br><br>
 
 
 </body></html>
