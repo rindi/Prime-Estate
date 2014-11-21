2e41290d71faa981c2ced8f3095749ae2969f612
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-11-21 08:30:06 +0000

    fixed sizing issue with profile

--- m3/views/profile.php
+++ m3/views/profile.php
@@ -31,8 +31,9 @@ $profile = $profilecontroller->getProfile(($id));
     <!--<div class="row">-->
     <!--<div class="col-md-4 col-md-offset-4">-->
     <div class="container-fluid">
-        <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+        <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
             <div class="panel panel-primary">
+                		
                 <div class="panel-heading">
                     <h3 class="panel-title"><?php echo $userid;?>'s Profile</h3>
                 </div>
