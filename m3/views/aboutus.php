5731004d6d8b5c9df9b10721bf200de356b92f9d
jolson <jolson@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-12-01 20:45:43 +0000

    added about us page, needs styling

---
+++ m3/views/aboutus.php
@@ -0,0 +1,122 @@
+<?php
+include 'navbar.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="../../favicon.ico">
+
+	<title>PrimeEstate - About Us</title>
+
+	<!-- Custom styles for this template -->
+	<link href="cover.css" rel="stylesheet">
+
+	<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+	<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+	<script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+	<link rel="stylesheet" href="http://sfsuswe.com/~f14g03/views/css/index.css" />
+	<link rel="stylesheet" href="./views/css/cover.css" />
+	<style>
+	    body, html { 
+		overflow-x: hidden; 
+		overflow-y: auto;
+	    }
+	</style>
+
+	
+    </head>
+
+    <body>
+	<div class="site-wrapper">
+	    <div class="site-wrapper-inner">
+		<div class="cover-container">
+		    <div class="inner cover">
+			<h1 class="cover-heading">About PrimeEstate</h1>
+                        <p>
+                            PrimeEstate is a demo website created for San Francisco State University 
+                            Computer Science's Fall 2014 Section of Software Engineering.  
+                            Our group consists of 5 software engineers.  Below you will find links to 
+                            the website's project specifications, and you will be able 
+                            to review each of the five milestones we have released.  
+                        </p>
+		    </div>
+		    <hr>
+		    <!-- Three columns of text below the carousel -->
+		    <div class="row" style="padding-top: 40px;">
+			<div class="col-lg-2">
+			    <img class="img-circle" src="assets/images/foralex.gif" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+			    <h2>Dong</h2>
+			    <p>
+                                Frontend/Scripting 
+                            </p>
+			</div><!-- /.col-lg-4 -->
+                        <div class="col-lg-2">
+			    <img class="img-circle" src="assets/images/foralex.gif" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+			    <h2>Rushab</h2>
+			    <p>
+                                Frontend Lead/Subversion Administrator  
+                            </p>
+			</div><!-- /.col-lg-4 -->
+                        		<div class="col-lg-2">
+			    <img class="img-circle" src="assets/images/foralex.gif" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+			    <h2>Yi-Hsien</h2>
+			    <p>
+                                Frontend/User Experience 
+                            </p>
+			</div><!-- /.col-lg-4 -->
+                        <div class="col-lg-2">
+			    <img class="img-circle" src="assets/images/foralex.gif" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+			    <h2>Alex</h2>
+			    <p>
+                                Backend Lead/Database Specialist
+                            </p>
+			</div><!-- /.col-lg-4 -->
+                        		<div class="col-lg-2">
+			    <img class="img-circle" src="assets/images/jon.jpg" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+			    <h2>Jonathan</h2>
+			    <p>
+                                Team Lead/Backend 
+                            </p>
+			</div><!-- /.col-lg-4 -->
+                    
+		    </div><!-- /.row -->
+		</div>
+	    </div>
+	</div>
+
+	<!-- Bootstrap core JavaScript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="../../dist/js/bootstrap.min.js"></script>
+	<script src="../../assets/js/docs.min.js"></script>
+	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+	<script>
+            (function () {
+                'use strict';
+                if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+                    var msViewportStyle = document.createElement('style')
+                    msViewportStyle.appendChild(
+                            document.createTextNode(
+                                    '@-ms-viewport{width:auto!important}'
+                                    )
+                            )
+                    document.querySelector('head').appendChild(msViewportStyle)
+                }
+            })();
+	</script>
+    </body>
+</html>
