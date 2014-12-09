c1d27e7c13d529b2ff83f4e7e0373261608c31da
rindi <rindi@users.noreply.github.com>
2014-12-09 19:50:47 +0000

    Testing addition of Google Analytics

--- m3/views/navbar.php
+++ m3/views/navbar.php
@@ -12,10 +12,18 @@ if (isset($_SESSION['type'])) {
     <link rel="shortcut icon" href="http://sfsuswe.com/~f14g03/views/assets/logo/favicon.ico" type="image/x-icon">
     <link rel="icon" href="http://sfsuswe.com/~f14g03/views/assets/logo/favicon.ico" type="image/x-icon">
     <head>
-    </head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-57497318-1', 'auto');
+        ga('send', 'pageview');
+    </script>
     <link rel="stylesheet" href="http://sfsuswe.com/~f14g03/views/css/bootstrap.css" />
     <link rel="stylesheet" href="css/main.css" />
     <style>
@@ -33,6 +41,7 @@ if (isset($_SESSION['type'])) {
             border-bottom: 2px solid #12cac5;
         }
     </style>
+    </head>
     <body>
         <nav class="navbar" role="navigation" style="margin-bottom:0px;">
             <div class="navbar-header" >
