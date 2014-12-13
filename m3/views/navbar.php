98f54d0303e04a07359526f0c2c460b1235ebdf1
rindi <rindi@users.noreply.github.com>
2014-12-13 03:47:35 +0000

    Formatted navbar code

--- m3/views/navbar.php
+++ m3/views/navbar.php
@@ -9,38 +9,42 @@ if (isset($_SESSION['type'])) {
     $type = 0;
 ?>
 <html>
-    <link rel="shortcut icon" href="http://sfsuswe.com/~f14g03/views/assets/logo/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="http://sfsuswe.com/~f14g03/views/assets/logo/favicon.ico" type="image/x-icon">
     <head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    
+        <link rel="shortcut icon" href="http://sfsuswe.com/~f14g03/views/assets/logo/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="http://sfsuswe.com/~f14g03/views/assets/logo/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="http://sfsuswe.com/~f14g03/views/css/bootstrap.css" />
+        <link rel="stylesheet" href="css/main.css" />
 
-        ga('create', 'UA-57497318-1', 'auto');
-        ga('send', 'pageview');
-    </script>
-    <link rel="stylesheet" href="http://sfsuswe.com/~f14g03/views/css/bootstrap.css" />
-    <link rel="stylesheet" href="css/main.css" />
-    <style>
-        .navbar{
-            margin-bottom: 0px;
-        }
-        .footer {
-            background-color: #FFFFFF;
-            text-align: center;
-        }
-        .navbar{
-            background: rgba(255,255,255, 0.9);
-            border-radius: 0px;
-            border: 0px;
-            border-bottom: 2px solid #12cac5;
-        }
-    </style>
+        <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-57497318-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+        
+        <style>
+            .navbar{
+                margin-bottom: 0px;
+            }
+            .footer {
+                background-color: #FFFFFF;
+                text-align: center;
+            }
+            .navbar{
+                background: rgba(255,255,255, 0.9);
+                border-radius: 0px;
+                border: 0px;
+                border-bottom: 2px solid #12cac5;
+            }
+        </style>
+        
     </head>
     <body>
         <nav class="navbar" role="navigation" style="margin-bottom:0px;">
@@ -57,82 +61,78 @@ if (isset($_SESSION['type'])) {
             <div id="navbar-collapse" class="navbar-collapse collapse">
                 <strong>
                     <ul class="nav navbar-nav navbar-left"> 
-<?php
-if ($type == 1) {
-    ?>
-                            <li class="dropdown">
-                                <a class="dropdown-toggle" data-toggle="dropdown">My Profile<span class="caret"></span></a>
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="http://sfsuswe.com/~f14g03/views/profile.php">Profile</a></li>
-                                    <li><a href="http://sfsuswe.com/~f14g03/views/changepassword.php">Change password</a></li>
-                                    <!--                                    <li class="divider"></li>
-                                                                        <li><a href="#">Separated link</a></li>-->
-                                </ul>
-                            </li>
-                            <li><a href="http://sfsuswe.com/~f14g03/views/buyers_guide.php">Buyer's Guide</a>
-                            </li>
-
-    <?php
-}
-if ($type == 0 || $type == 1) {
-    ?>
-                            <li> 
-                                <a href="http://sfsuswe.com/~f14g03/index.php">Buy Your New Home</a>
-                            </li>
-                            <li> 
-                                <a href="http://sfsuswe.com/~f14g03/views/sell.php">Sell Your Home Today!</a>
-                            </li>
-                            <li> 
-                                <a href="http://sfsuswe.com/~f14g03/views/contactus.php">Contact Us</a>
-                            </li><?php
-}
-if ($type == 2) {
-    ?>
-                            <li>
-                                <a href="http://sfsuswe.com/~f14g03/views/leads.php">Leads</a>
-                            </li><?php
-}
-if ($type == 2) {
-    ?>
-                            <li>
-                                <a href="http://sfsuswe.com/~f14g03/views/search_results.php?rid=1">Dashboard</a>
-                            </li><?php
-                    }
-                    if ($type == 2) {
-                        ?>
-                            <li>
-                                <a href="http://sfsuswe.com/~f14g03/views/add_listing.php">Add Listing</a>
-                            </li><?php
-                    }
-                    if ($type == 3) {
-                        ?>
-                            <li>
-                                <a href="http://sfsuswe.com/~f14g03/views/admin.php">Admin</a>
-                            </li><?php }
-?>
+                        <?php
+                        if ($type == 1) {?>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown">My Profile<span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="http://sfsuswe.com/~f14g03/views/profile.php">Profile</a></li>
+                                <li><a href="http://sfsuswe.com/~f14g03/views/changepassword.php">Change password</a></li>
+                                <!--                                    <li class="divider"></li>
+                                                                    <li><a href="#">Separated link</a></li>-->
+                            </ul>
+                        </li>
+                        <li><a href="http://sfsuswe.com/~f14g03/views/buyers_guide.php">Buyer's Guide</a></li>
+                        <?php
+                            }
+                            if ($type == 0 || $type == 1) {?>
+                        <li> 
+                            <a href="http://sfsuswe.com/~f14g03/index.php">Buy Your New Home</a>
+                        </li>
+                        <li> 
+                            <a href="http://sfsuswe.com/~f14g03/views/sell.php">Sell Your Home Today!</a>
+                        </li>
+                        <li> 
+                            <a href="http://sfsuswe.com/~f14g03/views/contactus.php">Contact Us</a>
+                        </li>
+                            <?php
+                            }
+                            if ($type == 2) { ?>
+                        <li>
+                            <a href="http://sfsuswe.com/~f14g03/views/leads.php">Leads</a>
+                        </li>
+                        <?php
+                            }
+                            if ($type == 2) { ?>
+                        <li>
+                            <a href="http://sfsuswe.com/~f14g03/views/search_results.php?rid=1">Dashboard</a>
+                        </li><?php
+                        }
+                        if ($type == 2) { ?>
+                        <li>
+                            <a href="http://sfsuswe.com/~f14g03/views/add_listing.php">Add Listing</a>
+                        </li>
+                            <?php
+                            }
+                            if ($type == 3) { ?>
+                        <li>
+                            <a href="http://sfsuswe.com/~f14g03/views/admin.php">Admin</a>
+                        </li>
+                            <?php } ?>
                     </ul>
-                    <ul     class="nav navbar-nav navbar-right">
+                    <ul class="nav navbar-nav navbar-right">
                         <?php
                         if ($type == 1 || $type == 2 || $type == 3) {
                             ?>
                             <li> 
                                 <a href="http://sfsuswe.com/~f14g03/views/logout.php">Logout</a>
-                            </li><?php } ?>
-<?php
-if ($type == 0) {
-    ?>
-                            <li>
-                                <a href="http://sfsuswe.com/~f14g03/views/registration.php">Sign Up</a>
                             </li>
-
-    <?php
-}
-if ($type == 0) {
-    ?>
-                            <li>
-                                <a href="http://sfsuswe.com/~f14g03/views/newlogin.php">Login</a>
-                            </li>
-<?php } ?></ul>
+                        <?php } ?>
+                        <?php
+                        if ($type == 0) {
+                        ?>
+                        <li>
+                            <a href="http://sfsuswe.com/~f14g03/views/registration.php">Sign Up</a>
+                        </li>
+                        <?php
+                        }
+                        if ($type == 0) {
+                        ?>
+                        <li>
+                            <a href="http://sfsuswe.com/~f14g03/views/newlogin.php">Login</a>
+                        </li>
+                        <?php }?>
+                    </ul>
                 </strong>
             </div>
         </nav>
