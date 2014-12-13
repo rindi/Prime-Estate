df7d066f6acea5713dcd48487e6a516276b9dd8d
rindi <rindi@users.noreply.github.com>
2014-12-13 18:53:27 +0000

    Forgot password integration test

--- m3/views/newlogin.php
+++ m3/views/newlogin.php
@@ -42,44 +42,55 @@
 	<div id="loginModal" class="modal show" role="dialog" aria-hidden="true">
 	    <div class="modal-dialog">
 		<div class="modal-content">
+                    <div class="col-md-12">
+			    <button class="close" data-dismiss="modal" aria-hidden="true" onClick="history.go(-1);return true;">X</button>
+                    </div>
 		    <div class="modal-header">
 			<h1 class="text-center">Sign in to your account</h1>
 		    </div>
 		    <div class="modal-footer">
 			<form class="form col-md-12 center-block" id="logform" method="post" action="loginhandler.php" name="loginform">
 			    <div class="form-group">
-				<input type="text" class="form-control input-lg" name="login_username" placeholder="Username" required>
+				<input type="text" class="form-control input-lg" name="login_username" id="login_username" placeholder="Username" required>
 			    </div>
 			    <div class="form-group">
 				<input type="password" class="form-control input-lg" name="login_password" placeholder="Password" required>
 			    </div>
 			    <div class="form-group" style="margin-bottom: 10px">
 				<button class="btn btn-default btn-lg btn-block"><strong>LOGIN</strong></button>
-				<br/>
-                                <span class="pull-left">
-                                    <button class="btn btn-default btn-lg" onclick="submitForm('registration.php')" >
-                                        <strong>Not registered? Sign up! </strong>
-                                    </button>
-				</span>
-                                <span class="pull-right">
+                                <br/>
+                            </div>
+                        </form>
+                        <div>
+                            <span class="pull-left">
+                                <button class="btn btn-default btn-lg" onclick="location.href='registration.php'" >
+                                    <strong>Not registered? Sign up! </strong>
+                                </button>
+                            </span>
+                            <span class="pull-right">
+                                <form id="forgotform" method="post" action="forgotpassword.php" name="forgotform">
+                                    <input type="hidden" name = "login_username" id="userforforgotpassword"/>
                                     <button class="btn btn-default btn-lg" onclick="submitForm('forgotpassword.php')" >
                                         <strong>Forgot Password? </strong>
                                     </button>
-				</span>
-			    </div>
-			</form>
-			<div class="col-md-12">
-			    <button class="close" data-dismiss="modal" aria-hidden="true" onClick="history.go(-1);
-                    return true;">Cancel</button>
-			</div>	
-		    </div>
-		</div>
-	    </div>
-	</div>
+                                </form>
+                            </span>
+			</div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-	<!-- jQuery -->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-	<!-- Bootstrap Core JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <!-- jQuery -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+    <script>
+    function getusername() {
+        var username = document.getElementById("login_username").value;
+        document.getElementById("userforforgotpassword").innerHTML = username;
+    }
+    </script>
     </body>
 </html>
