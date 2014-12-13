75ea8bd8cb4587338985c6be7a95419e52e7452d
rindi <rindi@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-12-13 10:08:12 +0000

    Incorrect answer doesnt reveal new password.

--- m3/views/forgotpasswordchanger.php
+++ m3/views/forgotpasswordchanger.php
@@ -10,18 +10,17 @@ $username = $_POST['username'];
 $answer = $_POST['answer'];
 
 $temppass = base64_encode($username);
-echo $temppass;
 $temppassdb = md5($temppass);
 $usercontroller = new users_controller();
 $userquestion = $usercontroller->getUserQuestion($username);
 $reganswer = $usercontroller->getUserAnswer($username);
 $input['username'] = $username;
 $input['password'] = $temppassdb;
-        
+$flag=0;        
 if($answer==$reganswer)
 {
+    $flag=1;
     $usercontroller->changePassword($input);
-    echo "Password changed";
 }
 
 ?>
@@ -30,11 +29,17 @@ if($answer==$reganswer)
     <div class="container-fluid">
     <div id="box" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2" >
     <div class="panel-heading" style="border: 1px solid;border-color: 000;">
-        <h3 class="panel-title" style="color: fff;"></h3>
+        <h3 class="panel-title" style="color: fff;"><?php if($flag==1)echo "Success";?></h3>
     </div>
     <div class="panel-body" style="border: 1px solid;border-color:#12ACA5;">
-
-    </form>
+        <h2>
+            <?php
+            if ($flag==1)
+                echo "<h2>".$username.", your new password is ".$temppass."</h2>";
+            else
+                echo"<h2>Your answer is incorrect</h2>";
+            ?>
+        </h2>
     </div>
     </div>    
     </div>
