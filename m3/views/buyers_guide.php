edd8a59bbeb43946700b9b2c5c024a1587943935
rindi <rindi@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2014-12-19 20:13:50 +0000

    Consistent styling for buyers guide

--- m3/views/buyers_guide.php
+++ m3/views/buyers_guide.php
@@ -1,93 +1,102 @@
 <!DOCTYPE html>
 <?php
-
 /*
-@(#)File:           Buyers Guide
-@(#)Purpose:        Buyers Guide Information
-@(#)Author:         PrimeEstate
-@(#)Product:        PrimeEstate Website 2014
-*/
+  @(#)File:           Buyers Guide
+  @(#)Purpose:        Buyers Guide Information
+  @(#)Author:         PrimeEstate
+  @(#)Product:        PrimeEstate Website 2014
+ */
 
 include 'navbar.php';
 ?>
 <html>
     <head>
         <meta/>
-  <title>PrimeEstate - Guide to purchasing a home</title>
-</head>
-<body>
+        <title>PrimeEstate - Guide to purchasing a home</title>
+    </head>
+    <body>
 
-<div style="margin: auto; width: 90%; max-width: 1000px; background: #FCFCFC; padding: 15px">
+        <div class="container-fluid">
+            <div id="box" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2" >
+                <div class="panel-heading" style="border: 1px solid;border-color: 000;">
+                    <h3 class="panel-title" style="color: #000;text-align: center;">Factors to Consider when Buying a House</h3>
+                </div>
 
-<center><h2>Factors to Consider when Buying a House</h2></center>
+                <p>Most people are looking forward to to have their own dream houses in their live. Also, they usually spend most of their savings in their live to buy their favourite houses. Therefore, it is very important to get everything ready before buying houses.   Especially, When buyers can clearly understand  some the knowledge of house purchasing  which can let buyers likely protect their own  benefits and reduce their  unnecessary money and time lost.</p>
 
-<p>Most people are looking forward to to have their own dream houses in their live. Also, they usually spend most of their savings in their live to buy their favourite houses. Therefore, it is very important to get everything ready before buying houses.   Especially, When buyers can clearly understand  some the knowledge of house purchasing  which can let buyers likely protect their own  benefits and reduce their  unnecessary money and time lost.</p>
+                <h4><span class="club"></span> The guides of looking at your houses  </h4>
 
-<h4><span class="club">&#8962;</span> The guides of looking at your houses  </h4>
+                <p>(A) By the general environments: regional environment, transportation network, the type of housing, public works, schools, market and other factors.</p>
+                <p>(B) By the narrow environments: neighborhood, public facilities, parking lots, housing conditions and other factors.</p>
+                <div class="panel-heading" style="border: 1px solid;border-color: 000;">
+                    <h3 class="panel-title" style="color: #000;text-align: center;"> A. General environment</h3>
+                </div>
+                <ol>
+                    <li>Regional environment</li>
+                    <ul>
+                        <li>Neighborhood quality of life is good? </li>
+                        <li>Standards of residential construction?</li>
+                        <li>Adult industry around?</li>
+                        <li>Serious river pollution?</li>
+                        <li>Noise pollution like railroad?</li> 
+                        <li>Any high-voltage tower nearby? </li>
+                        <li>Graveyard around?</li>
+                        <li>Gas stations in the vicinity?</li>
+                    </ul><br/>
+                    <li>Transportation network</li>
+                    <ul> <li>Integrity of regional transportation system is directly related to the home buyers' work, school, and whether they easily and quickly reach their destinations. Therefore, in consideration of the traffic problem, homebuyers should understand the convenience of the conjunctions roads, so that it is easy to control the good traffic conditions every day and prevent wasting too much time.
+                        </li></ul><br/>
+                    <li>Types of houses</li>
+                    <ul> 
+                        <li>Single Hose : No HOA fee. </li>
+                        <li>Townhouse :  HOA fee.</li>
+                        <li>Condominium : Townhouse : HOA fee.</li>
+                        <li>*Homeowners Association Fee (HOA Fee) : An amount of money that must be paid monthly   
+                            by owners of certain types of residential property to an organization that assists with 
+                            maintaining and improving that property and others.</li>
+                    </ul><br/> 
 
-<p>(A) By the general environments: regional environment, transportation network, the type of housing, public works, schools, market and other factors.</p>
-<p>(B) By the narrow environments: neighbourhood, public facilities, parking lots, housing conditions and other factors.</p>
+                    <li>Public Works</li>
+                    <li>Home buyers should consider value of the house in future, and then they should pay attention to the living conditions of future or existing adequacy of public construction with the government, such as ? hospitals, parks, stadiums, community centers, cultural centers, art galleries, super market, playground, child-care centers and so on.</li>
 
-<h4><span class="club">&#8962;</span> (A) General environment</h4>
-<ol>
-    <li>Regional environment</li>
-    <ul>
-<li>Neighborhood quality of life is good? </li>
-<li>Standards of residential construction?</li>
-<li>Porn industry around?</li>
-<li>Serious river pollution?</li>
-<li>Noise pollution like railroad?</li> 
-<li>Any high-voltage tower nearby? </li>
-<li>Grave area?</li>
-<li>Gas stations?</li>
-</ul>
-<li>Transportation network</li>
-<ul> <li>Integrity of regional transportation system is directly related to the home buyers' work, school, and whether they easily and quickly reach their destinations. Therefore, in consideration of the traffic problem, homebuyers should understand the convenience of the conjunctions roads, so that it is easy to control the good traffic conditions every day and prevent wasting too much time.
-    </li></ul>
-<li>Types of houses</li>
-<ul> 
-<li>Single Hose: No HOA fee. </li>
-<li>Townhouse:  HOA fee.</li>
-<li>Condominium: Townhouse: HOA fee.</li>
-<li>***HOMEOWNERS ASSOCIATION FEE (HOA FEE) : An amount of money that must be paid monthly   
-    by owners of certain types of residential property to an organization that assists with 
-    maintaining and improving that property and others.</li>
-	</ul> 
+                    <li>Schools and markets </li>
+                    <li>Good reputation </li><ul><li>Schools not only provide a good educational environment but also increase the value of properties in the future.</li>
+                        <li>If homes can nearby markets, supermarkets, shopping centers, any busy professional women, can also reduce the number of unnecessary time.</p>
+                    </ul><br/>
+                </ol>
+                <div class="panel-heading" style="border: 1px solid;border-color: 000;">
+                    <h3 class="panel-title" style="color: #000;text-align: center;"> B. Narrow environments</h3>
+                </div>
+                <ol>
+                    <li>Regional environment
+                    </li>
 
-<li>Public Works</li>
-<li>Home buyers should consider value of the house in future, and then they should pay attention to the living conditions of future or existing adequacy of public construction with the government, such as ? hospitals, parks, stadiums, community centers, cultural centers, art galleries, super market, playground, child-care centers and so on.</li>
+                    <p>For some purposes around the house environment, home buyers should go in the daytime and at night to make an observation, include - noise, garbage problem, vendors like pub and so on.</p>
 
-<li>Schools and markets </li>
-<li>Good reputation </li><ul><li>Schools not only provide a good educational environment but also increase the value of properties in the future.</li>
-<li>If homes can nearby markets, supermarkets, shopping centers, any busy professional women, can also reduce the number of unnecessary time.</p>
-    </ul>
-<h4><span class="club">&#8962;</span> (B)Narrow environments</h4>
-
-<p>1.Regional environment</p>
-
-<p>For some purposes around the house environment, home buyers should go in the daytime and at night to make an observation, include - noise, garbage problem, vendors like pub and so on.</p>
-
-<p>2.Parking</p>
-<p>Parking is a big issue which has been troubled by modern families.</p>
-<p>Home buyers should check how easy the street parking is.</p>
-<p>Parking is exclusive or shared.</p>
-<p>Home buyers need to figure how much a parking spot costs if they need.</p>
-
-<h4><span class="club">&#8962;</span> House Mortgage/Loan</h4>
-<p>Basically, a default value of rate is 30 years of 6.5%, but actually home buyers' home loan interest rate base on your credit history and other various conditions. Each person's rates are not the same. Generally if credit score is not high enough, the loan interest rate is relatively higher. Before you loan, the bank will first consult your consent and check your credit score.</p>
-
-<p>&#10045;Credit score list</p>
-<p>750 ~ 850:Excellent.</p>
-<p>660 ~ 749:Good. </p>
-<p>620 ~ 659:Fair. </p>
-<p>350 ~ 619:Poor.</p>
-
-<p>On the other hand, if you are a first time home buyer, you can get a lower interest rate with first-time home buyer education program.</p>
-
-<!--<div style="text-align: center; background: #826B34"><a href=""><img src="" border="0" alt=""></a></div>
-            <input class="btn btn-default" style="float: left;" type="button" value="Back" onClick="history.go(-1);return true;">
-</div>-->
-    <br><br><br>
+                    <li>Parking</li>
+                    <ul><li>Parking is a big issue which has been troubled by modern families.</li>
+                        <li>Home buyers should check how easy the street parking is.</li>
+                        <li>Parking is exclusive or shared.</li>
+                        <li>Home buyers need to figure how much a parking spot costs if they need.</li>
+                    </ul><br/>            </ol>
 
 
-</body></html>
+                <div class="panel-heading" style="border: 1px solid;border-color: 000;">
+                    <h3 class="panel-title" style="color: #000;text-align: center;"> C. House Mortgage/Loan</h3>
+                </div>                <p>Basically, a default value of rate is 30 years of 6.5%, but actually home buyers' home loan interest rate base on your credit history and other various conditions. Each person's rates are not the same. Generally if credit score is not high enough, the loan interest rate is relatively higher. Before you loan, the bank will first consult your consent and check your credit score.</p>
+
+                <p>Credit score list</p>
+                <ul>
+                    <li>750 ~ 850 : Excellent.</li>
+                    <li>660 ~ 749 : Good. </li>
+                    <li>620 ~ 659 : Fair. </li>
+                    <li>350 ~ 619 : Poor.</li>
+                </ul><br/>
+                <p>On the other hand, if you are a first time home buyer, you can get a lower interest rate with first-time home buyer education program.</p>
+            <!--<div style="text-align: center; background: #826B34"><a href=""><img src="" border="0" alt=""></a></div>
+                        <input class="btn btn-default" style="float: left;" type="button" value="Back" onClick="history.go(-1);return true;">
+            </div>-->
+                <br><br><br>
+
+
+                </body></html>
