2259ca87e2e5eb970fff20bd21705db089bb4708
rindi <rindi@d1dcd6a2-6008-470b-adcc-6b569fd64225>
2015-11-25 00:50:47 +0000

    Delete old tag by svn-tag Jenkins plugin.

--- last-successful/rindi_demo/last-successful/rindi_demo/last-successful/rindi_demo/m3/views/interest.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../controllers/interest_controller.php";
-if(isset($_GET['interest']))
-{
-    $interest = new interest_controller();
-    $interest->expressInterest(551,55);
-}
-
-?>
